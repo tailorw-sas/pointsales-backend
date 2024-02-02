92354cf492ee518981c409871b0cf15fb7f23f15
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2024-02-02 12:29:35 -0500

    Se arregla la forma de responder el login del gateway

--- gateway/src/main/java/com/kynsoft/service/IKeycloakService.java
+++ gateway/src/main/java/com/kynsoft/service/IKeycloakService.java
@@ -2,6 +2,7 @@ package com.kynsoft.service;
 
 import com.kynsoft.dto.LoginDTO;
 import com.kynsoft.dto.RegisterDTO;
+import com.kynsoft.dto.TokenResponse;
 import org.keycloak.representations.idm.UserRepresentation;
 import reactor.core.publisher.Mono;
 
@@ -9,7 +10,7 @@ import java.util.List;
 
 public interface IKeycloakService {
 
-	Mono<String> authenticate(LoginDTO loginDTO);
+	Mono<TokenResponse> authenticate(LoginDTO loginDTO);
     List<UserRepresentation> findAllUsers();
     List<UserRepresentation> searchUserByUsername(String username);
     int countUsers();
