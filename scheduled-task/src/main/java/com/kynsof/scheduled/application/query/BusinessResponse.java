25f88402a017b3617516e0e9144bb17adfcd2b68
Yannier <penaescalonayannier@gmail.com>
2024-02-02 19:13:00 -0500

    add business response entity

---
+++ scheduled-task/src/main/java/com/kynsof/scheduled/application/query/BusinessResponse.java
@@ -0,0 +1,38 @@
+package com.kynsof.scheduled.application.query;
+
+import com.kynsof.scheduled.domain.dto.BusinessDto;
+import com.kynsof.scheduled.domain.dto.EBusinessStatus;
+import com.kynsof.scheduled.infrastructure.config.bus.query.IResponse;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class BusinessResponse implements IResponse {
+    private UUID id;
+    private String name;
+    private String description;
+    private byte[] logo;
+    private String ruc;
+    private EBusinessStatus status;
+    private LocalDateTime createAt;
+    private LocalDateTime updateAt;
+    private LocalDateTime deleteAt;
+
+    public BusinessResponse(BusinessDto object) {
+        this.id = object.getId();
+        this.name = object.getName();
+        this.description = object.getDescription();
+        this.logo = object.getLogo();
+        this.ruc = object.getRuc();
+        this.status = object.getStatus();
+        this.createAt = object.getCreateAt();
+        this.updateAt = object.getUpdateAt();
+        this.deleteAt = object.getDeleteAt();
+    }
+
+}
