cde03009a1bff3d4f0bbaf4d831cedfafbadf8ee
Yannier <penaescalonayannier@gmail.com>
2024-02-02 19:30:33 -0500

    add controller and create

---
+++ scheduled-task/src/main/java/com/kynsof/scheduled/controller/BusinessController.java
@@ -0,0 +1,46 @@
+package com.kynsof.scheduled.controller;
+
+
+import com.kynsof.scheduled.application.PaginatedResponse;
+import com.kynsof.scheduled.application.command.business.create.CreateBusinessCommand;
+import com.kynsof.scheduled.application.command.business.create.CreateBusinessMessage;
+import com.kynsof.scheduled.application.command.business.create.CreateBusinessRequest;
+import com.kynsof.scheduled.application.command.qualification.create.CreateQualificationCommand;
+import com.kynsof.scheduled.application.command.qualification.create.CreateQualificationMessage;
+import com.kynsof.scheduled.application.command.qualification.create.CreateQualificationRequest;
+import com.kynsof.scheduled.application.command.qualification.delete.QualificationDeleteCommand;
+import com.kynsof.scheduled.application.command.qualification.delete.QualificationDeleteMessage;
+import com.kynsof.scheduled.application.command.qualification.update.UpdateQualificationCommand;
+import com.kynsof.scheduled.application.command.qualification.update.UpdateQualificationMessage;
+import com.kynsof.scheduled.application.command.qualification.update.UpdateQualificationRequest;
+import com.kynsof.scheduled.application.query.QualificationResponse;
+import com.kynsof.scheduled.application.query.qualification.getAll.FindQualificationWithFilterQuery;
+import com.kynsof.scheduled.application.query.qualification.getbyid.FindQualificationByIdQuery;
+
+import com.kynsof.scheduled.infrastructure.config.bus.IMediator;
+import java.util.UUID;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/business")
+public class BusinessController {
+
+    private final IMediator mediator;
+
+    public BusinessController(IMediator mediator){
+
+        this.mediator = mediator;
+    }
+
+    @PostMapping("")
+    public ResponseEntity<CreateBusinessMessage> create(@RequestBody CreateBusinessRequest request)  {
+        CreateBusinessCommand createCommand = CreateBusinessCommand.fromRequest(request);
+        CreateBusinessMessage response = mediator.send(createCommand);
+
+        return ResponseEntity.ok(response);
+    }
+
+}
