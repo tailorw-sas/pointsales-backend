8e5b25ababf647fcc0c2dd842ef2111c37eedaea
Yannier <penaescalonayannier@gmail.com>
2024-02-10 16:14:33 -0500

    test

--- scheduled-task/src/main/java/com/kynsof/scheduled/ScheduledApplication.java
+++ scheduled-task/src/main/java/com/kynsof/scheduled/ScheduledApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ScheduledApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ScheduledApplication.class, args);
     }
