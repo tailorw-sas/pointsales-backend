bed2ee6b0babc47210c03eb2c31b3843d0cd5d83
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2024-02-15 07:05:06 -0500

    Merge remote-tracking branch 'origin/dev' into dev

--- patients/src/main/java/com/kynsof/patients/application/query/patients/getall/GetAllPatientsQueryHandler.java
+++ patients/src/main/java/com/kynsof/patients/application/query/patients/getall/GetAllPatientsQueryHandler.java
@@ -3,7 +3,6 @@ package com.kynsof.patients.application.query.patients.getall;
 import com.kynsof.patients.domain.bus.query.IQueryHandler;
 import com.kynsof.patients.domain.dto.PaginatedResponse;
 import com.kynsof.patients.domain.service.IPatientsService;
-import com.kynsof.patients.infrastructure.services.PatientsServiceImpl;
 import org.springframework.stereotype.Component;
 
 @Component
