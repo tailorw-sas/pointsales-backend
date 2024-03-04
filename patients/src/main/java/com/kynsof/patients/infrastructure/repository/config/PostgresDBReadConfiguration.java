f50091c438f1e0ee05e12c9e81026c78a442c780
Yannier <70853534+penaescalonayannier@users.noreply.github.com>
2024-03-04 15:46:59 -0500

    adicione command query en patients

---
+++ patients/src/main/java/com/kynsof/patients/infrastructure/repository/config/PostgresDBReadConfiguration.java
@@ -0,0 +1,50 @@
+package com.kynsof.patients.infrastructure.repository.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import jakarta.persistence.EntityManagerFactory;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(entityManagerFactoryRef = "readEntityManagerFactory", transactionManagerRef = "readTransactionManager", basePackages = {
+        "com.kynsof.patients.infrastructure.repositories.query" })
+public class PostgresDBReadConfiguration {
+
+    @Bean(name = "readDataSourceProperties")
+    @ConfigurationProperties("spring.read-datasource")
+    public DataSourceProperties dataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean(name = "readDataSource")
+    @ConfigurationProperties(prefix = "spring.read-datasource")
+    public DataSource datasource(@Qualifier("readDataSourceProperties") DataSourceProperties properties) {
+        return properties.initializeDataSourceBuilder().build();
+    }
+
+    @Bean(name = "readEntityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean(EntityManagerFactoryBuilder builder,
+            @Qualifier("readDataSource") DataSource dataSource) {
+        return builder.dataSource(dataSource)
+                .packages("com.kynsof.patients.infrastructure.entity")
+                .persistenceUnit("ReadDB").build();
+    }
+
+    @Bean(name = "readTransactionManager")
+    public PlatformTransactionManager transactionManager(
+            @Qualifier("readEntityManagerFactory") EntityManagerFactory entityManagerFactory) {
+        return new JpaTransactionManager(entityManagerFactory);
+    }
+}
