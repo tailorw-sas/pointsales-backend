d474f079df17865f0f79f05529cfe7d323b98183
Yannier <70853534+penaescalonayannier@users.noreply.github.com>
2024-03-05 09:06:40 -0500

    refactor

--- calendar/src/main/java/com/kynsof/calendar/infrastructure/repository/command/UserWriteDataJPARepository.java
+++
@@ -1,9 +0,0 @@
-package com.kynsof.calendar.infrastructure.repository.command;
-
-import com.kynsof.calendar.infrastructure.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface UserWriteDataJPARepository extends JpaRepository<User, UUID> {
-}
