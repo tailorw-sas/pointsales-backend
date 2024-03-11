fbdd93e3370151b34d43da1f741bfe96e95e032a
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2024-03-11 14:09:33 -0500

    se agrega el metodo de obtener a una persona a partir de la identificacion

--- share/src/main/java/com/kynsof/share/core/infrastructure/ProducerSaveFileEventService.java
+++ share/src/main/java/com/kynsof/share/core/infrastructure/ProducerSaveFileEventService.java
@@ -1,4 +1,4 @@
-package com.kynsof.calendar.infrastructure.service.kafka.producer;
+package com.kynsof.share.core.infrastructure;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
