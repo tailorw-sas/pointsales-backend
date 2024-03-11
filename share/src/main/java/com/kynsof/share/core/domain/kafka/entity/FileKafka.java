e26162af4dc3b6c9c0c77889d724db7daebd9713
Yannier <70853534+penaescalonayannier@users.noreply.github.com>
2024-03-11 05:09:02 -0400

    adicione nuevas propiedades

--- share/src/main/java/com/kynsof/share/core/domain/kafka/entity/FileKafka.java
+++ share/src/main/java/com/kynsof/share/core/domain/kafka/entity/FileKafka.java
@@ -1,5 +1,6 @@
 package com.kynsof.share.core.domain.kafka.entity;
 
+import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,8 +10,9 @@ import lombok.Setter;
 @AllArgsConstructor
 public class FileKafka {
 
+    private UUID id;
+    private String microServiceName;
     private String fileName;
-
     private byte [] file;
 
     public FileKafka() {
