a3b7e7a95985b694e56526340d70157940d778de
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2024-03-13 16:01:00 -0500

    Se agrega el actualizar role del servicio identity

--- gateway/src/main/java/com/kynsoft/gateway/infrastructure/config/ApiRouteLocator.java
+++ gateway/src/main/java/com/kynsoft/gateway/infrastructure/config/ApiRouteLocator.java
@@ -6,6 +6,9 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import reactor.core.publisher.Flux;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 @AllArgsConstructor
 public class ApiRouteLocator implements RouteLocator {
 
@@ -19,10 +22,19 @@ public class ApiRouteLocator implements RouteLocator {
     RouteLocatorBuilder.Builder routesBuilder = routeLocatorBuilder.routes();
     
     for (RouteDTO route : updateRouteContext.getDefinitionsContext().getDefinitions()) {
-    	routesBuilder.route(route.getName(),
+
+        String path = route.getUri().toString();
+      URI uri;
+        try {
+        path =  path.replace("http","https");
+          uri = new URI(path);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+        routesBuilder.route(route.getName(),
                 r -> r.path(route.getPath())
                         .filters(f -> f.stripPrefix(1))
-                        .uri(route.getUri()));
+                        .uri(uri));
     }
     
     return routesBuilder.build().getRoutes();
