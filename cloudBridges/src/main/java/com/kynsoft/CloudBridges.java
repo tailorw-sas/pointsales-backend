fb1a49533dd7b16f8310261f1c5a516d4d2361eb
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2024-03-14 15:46:51 -0500

    Se agrega el actualizar role del servicio identity

--- cloudBridges/src/main/java/com/kynsoft/CloudBridges.java
+++ cloudBridges/src/main/java/com/kynsoft/CloudBridges.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ClientEmail {
+public class CloudBridges {
     public static void main(String[] args) {
-        SpringApplication.run(ClientEmail.class, args);
+        SpringApplication.run(CloudBridges.class, args);
     }
 }
