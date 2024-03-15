d3bfaf896c887870f3159c7adf88f499ac4aab3f
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2024-03-15 05:43:29 -0500

    Se agrega el actualizar role del servicio identity

---
+++ cloudBridges/src/main/java/com/kynsoft/notification/application/AppRestConfig.java
@@ -0,0 +1,14 @@
+package com.kynsoft.notification.application;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppRestConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
---
+++ cloudBridges/src/main/java/com/kynsoft/notification/controller/ImageController.java
@@ -0,0 +1,35 @@
+package com.kynsoft.notification.controller;
+
+
+import com.kynsoft.notification.application.ImageDownloadService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/images")
+public class ImageController {
+
+    private final ImageDownloadService imageDownloadService;
+
+    @Autowired
+    public ImageController(ImageDownloadService imageDownloadService) {
+        this.imageDownloadService = imageDownloadService;
+    }
+
+    @GetMapping("/get-image")
+    public ResponseEntity<byte[]> getImage(@RequestParam("id") UUID id) {
+        byte[] imageBytes = imageDownloadService.downloadImage(id);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .contentType(MediaType.IMAGE_JPEG) // Ajusta según el tipo de imagen
+                .body(imageBytes);
+    }
+}
