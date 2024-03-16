0e4952e9d3bc75f166a1e82f087ddaa9c5e72f38
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2024-03-16 17:16:58 -0500

    Prueba del deploiment

--- gateway/src/main/java/com/kynsoft/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/kynsoft/gateway/GatewayApplication.java
@@ -11,5 +11,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
+
     }
 }
