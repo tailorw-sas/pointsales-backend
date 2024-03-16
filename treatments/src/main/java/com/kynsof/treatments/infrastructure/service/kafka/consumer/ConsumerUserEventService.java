343553d79bac626aecbf67424217a0c9c22e44bc
Yannier <70853534+penaescalonayannier@users.noreply.github.com>
2024-03-16 15:14:25 -0400

    adicione evento para crear paciente en tratamiento

---
+++ treatments/src/main/java/com/kynsof/treatments/infrastructure/service/kafka/consumer/ConsumerUserEventService.java
@@ -0,0 +1,61 @@
+package com.kynsof.treatments.infrastructure.service.kafka.consumer;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kynsof.share.core.domain.kafka.entity.UserKafka;
+import com.kynsof.share.core.domain.kafka.event.EventType;
+import com.kynsof.treatments.domain.dto.PatientDto;
+import com.kynsof.treatments.domain.dto.enumDto.Status;
+import com.kynsof.treatments.domain.service.IPatientsService;
+import java.util.UUID;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Service
+public class ConsumerUserEventService {
+    @Autowired
+    private IPatientsService service;
+
+    // Ejemplo de un método listener
+    @KafkaListener(topics = "user", groupId = "user-treatments")
+    public void listen(String event) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode rootNode = objectMapper.readTree(event);
+
+            UserKafka eventRead = objectMapper.treeToValue(rootNode.get("data"), UserKafka.class);
+            EventType eventType = objectMapper.treeToValue(rootNode.get("type"), EventType.class);
+            if (eventType.equals(EventType.CREATED)) {
+                //Definir accion
+                this.service.create(new PatientDto(UUID.fromString(eventRead.getId()), "", eventRead.getFirstname(), eventRead.getLastname(), "", Status.ACTIVE, null));
+
+            }
+            if (eventType.equals(EventType.DELETED)) {
+                //Definir accion
+                System.err.println("#######################################################");
+                System.err.println("#######################################################");
+                System.err.println("SE EJECUTA UN DELETED");
+                System.err.println("#######################################################");
+                System.err.println("#######################################################");
+
+            }
+            if (eventType.equals(EventType.UPDATED)) {
+                //Definir accion
+                System.err.println("#######################################################");
+                System.err.println("#######################################################");
+                System.err.println("SE EJECUTA UN DELETED");
+                System.err.println("#######################################################");
+                System.err.println("#######################################################");
+
+            }
+        } catch (JsonProcessingException ex) {
+            Logger.getLogger(ConsumerUserEventService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+}
