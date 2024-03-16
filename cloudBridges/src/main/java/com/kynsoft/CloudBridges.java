d2252f5682376a4d016ae2e081c5a296f4283c85
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2024-03-16 06:34:10 -0500

    Se agrega el redis en la consulta de la imagen al servidor

--- cloudBridges/src/main/java/com/kynsoft/CloudBridges.java
+++ cloudBridges/src/main/java/com/kynsoft/CloudBridges.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CloudBridges {
     public static void main(String[] args) {
         SpringApplication.run(CloudBridges.class, args);
+
     }
 }
