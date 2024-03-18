7da5229021253c8584ae1095f43f706c9e596543
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2024-03-18 13:35:03 -0500

    Prueba del deploiment

--- gateway/src/main/java/com/kynsoft/gateway/infrastructure/config/CorsConfig.java
+++ gateway/src/main/java/com/kynsoft/gateway/infrastructure/config/CorsConfig.java
@@ -1,28 +1,28 @@
-package com.kynsoft.gateway.infrastructure.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-
-@Configuration
-public class CorsConfig {
-
-    @Bean
-    public CorsFilter corsFilter() {
-        CorsConfiguration config = new CorsConfiguration();
-
-
-        config.addAllowedOrigin("http://localhost:5173");
-        config.addAllowedMethod("*");
-        config.addAllowedHeader("*");
-
-        config.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config); // Aplica esta configuración a todas las rutas
-
-        return new CorsFilter(source);
-    }
-}
+//package com.kynsoft.gateway.infrastructure.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+//import org.springframework.web.filter.CorsFilter;
+//
+//@Configuration
+//public class CorsConfig {
+//
+//    @Bean
+//    public CorsFilter corsFilter() {
+//        CorsConfiguration config = new CorsConfiguration();
+//
+//
+//        config.addAllowedOrigin("http://localhost:5173");
+//        config.addAllowedMethod("*");
+//        config.addAllowedHeader("*");
+//
+//        config.setAllowCredentials(true);
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", config); // Aplica esta configuración a todas las rutas
+//
+//        return new CorsFilter(source);
+//    }
+//}
