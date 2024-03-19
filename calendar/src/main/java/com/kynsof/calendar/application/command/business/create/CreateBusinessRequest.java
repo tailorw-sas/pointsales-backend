13b4d36c13ba6997b8a2ee60c3b15ee6e04b4ee8
Yannier <70853534+penaescalonayannier@users.noreply.github.com>
2024-03-19 14:04:18 -0400

    fix

--- calendar/src/main/java/com/kynsof/calendar/application/command/business/create/CreateBusinessRequest.java
+++ calendar/src/main/java/com/kynsof/calendar/application/command/business/create/CreateBusinessRequest.java
@@ -1,12 +1,5 @@
 package com.kynsof.calendar.application.command.business.create;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Getter
-@Setter
-@NoArgsConstructor
 public class CreateBusinessRequest {
 
     private String name;
@@ -15,4 +8,57 @@ public class CreateBusinessRequest {
     private String description;
     private byte[] logo;
     private String ruc;
+
+    public CreateBusinessRequest() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public byte[] getLogo() {
+        return logo;
+    }
+
+    public void setLogo(byte[] logo) {
+        this.logo = logo;
+    }
+
+    public String getRuc() {
+        return ruc;
+    }
+
+    public void setRuc(String ruc) {
+        this.ruc = ruc;
+    }
+    
+    
 }
--- calendar/src/main/java/com/kynsof/calendar/application/command/schedule/create/CreateScheduleRequest.java
+++ calendar/src/main/java/com/kynsof/calendar/application/command/schedule/create/CreateScheduleRequest.java
@@ -1,16 +1,9 @@
 package com.kynsof.calendar.application.command.schedule.create;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.UUID;
 
-@Getter
-@Setter
-@AllArgsConstructor
 public class CreateScheduleRequest {
     private UUID resourceId;
     private UUID businessId;
@@ -19,4 +12,64 @@ public class CreateScheduleRequest {
     private LocalTime startTime;
     private LocalTime endingTime;
     private int stock;
+
+    public CreateScheduleRequest() {
+    }
+
+    public UUID getResourceId() {
+        return resourceId;
+    }
+
+    public void setResourceId(UUID resourceId) {
+        this.resourceId = resourceId;
+    }
+
+    public UUID getBusinessId() {
+        return businessId;
+    }
+
+    public void setBusinessId(UUID businessId) {
+        this.businessId = businessId;
+    }
+
+    public UUID getServiceId() {
+        return serviceId;
+    }
+
+    public void setServiceId(UUID serviceId) {
+        this.serviceId = serviceId;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalTime getEndingTime() {
+        return endingTime;
+    }
+
+    public void setEndingTime(LocalTime endingTime) {
+        this.endingTime = endingTime;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
 }
