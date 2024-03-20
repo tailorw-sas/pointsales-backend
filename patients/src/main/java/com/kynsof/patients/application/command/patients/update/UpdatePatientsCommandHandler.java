aef26e28da56d4a9232b7f216a917a67790fbe96
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2024-03-20 13:46:39 -0500

    Se cambia nombre del objeto contact info

--- patients/src/main/java/com/kynsof/patients/application/command/patients/update/UpdatePatientsCommandHandler.java
+++ patients/src/main/java/com/kynsof/patients/application/command/patients/update/UpdatePatientsCommandHandler.java
@@ -1,7 +1,11 @@
 package com.kynsof.patients.application.command.patients.update;
 
+import com.kynsof.patients.domain.dto.ContactInfoDto;
+import com.kynsof.patients.domain.dto.GeographicLocationDto;
 import com.kynsof.patients.domain.dto.PatientDto;
 import com.kynsof.patients.domain.dto.enumTye.Status;
+import com.kynsof.patients.domain.service.IContactInfoService;
+import com.kynsof.patients.domain.service.IGeographicLocationService;
 import com.kynsof.patients.domain.service.IPatientsService;
 import com.kynsof.share.core.domain.bus.command.ICommandHandler;
 import com.kynsof.share.core.domain.kafka.entity.FileKafka;
@@ -15,11 +19,15 @@ public class UpdatePatientsCommandHandler implements ICommandHandler<UpdatePatie
 
     private final IPatientsService serviceImpl;
     private final ProducerSaveFileEventService saveFileEventService;
+    private final IContactInfoService contactInfoService;
+    private final IGeographicLocationService geographicLocationService;
 
-    public UpdatePatientsCommandHandler(IPatientsService serviceImpl, ProducerSaveFileEventService saveFileEventService) {
+    public UpdatePatientsCommandHandler(IPatientsService serviceImpl, ProducerSaveFileEventService saveFileEventService, IContactInfoService contactInfoService, IGeographicLocationService geographicLocationService) {
         this.serviceImpl = serviceImpl;
 
         this.saveFileEventService = saveFileEventService;
+        this.contactInfoService = contactInfoService;
+        this.geographicLocationService = geographicLocationService;
     }
 
     @Override
@@ -46,6 +54,20 @@ public class UpdatePatientsCommandHandler implements ICommandHandler<UpdatePatie
                 idLogo,
                 command.getDisabilityType(),
                 command.getGestationTime()
+        ));
+
+        PatientDto patientDto = serviceImpl.findByIdSimple(command.getId());
+        GeographicLocationDto geographicLocationDto = geographicLocationService.findById(
+                command.getCreateContactInfoRequest().getGeographicLocationId());
+        UUID idContactId = contactInfoService.create(new ContactInfoDto(
+                UUID.randomUUID(),
+                patientDto,
+                command.getCreateContactInfoRequest().getEmail(),
+                command.getCreateContactInfoRequest().getEmail(),
+                command.getCreateContactInfoRequest().getAddress(),
+                command.getCreateContactInfoRequest().getBirthdayDate(),
+                Status.ACTIVE,
+                geographicLocationDto
         ));
 
     }
