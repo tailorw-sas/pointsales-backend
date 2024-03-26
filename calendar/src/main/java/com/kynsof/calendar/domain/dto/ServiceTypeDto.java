150c7a905455086d6883b780c4c8cf65c1a307cb
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2024-03-26 12:43:05 -0500

    Se agrega la foto para los servicios

--- calendar/src/main/java/com/kynsof/calendar/domain/dto/ServiceTypeDto.java
+++ calendar/src/main/java/com/kynsof/calendar/domain/dto/ServiceTypeDto.java
@@ -5,13 +5,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.io.Serializable;
 import java.util.UUID;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class ServiceTypeDto {
+public class ServiceTypeDto  implements Serializable {
     private UUID id;
     private String name;
     private UUID picture;
