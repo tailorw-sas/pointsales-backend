e45adc177d4affca2d2714b70fcea8019968bdb6
Yannier <70853534+penaescalonayannier@users.noreply.github.com>
2024-03-27 05:17:46 -0400

    personalizando BusinessRuleValidationException

--- calendar/src/main/java/com/kynsof/calendar/domain/rules/QualificationDescriptionMustBeNotNullRule.java
+++ calendar/src/main/java/com/kynsof/calendar/domain/rules/QualificationDescriptionMustBeNotNullRule.java
@@ -3,6 +3,7 @@ package com.kynsof.calendar.domain.rules;
 import com.kynsof.calendar.domain.dto.QualificationDto;
 import com.kynsof.calendar.domain.service.IQualificationService;
 import com.kynsof.share.core.domain.exception.DomainErrorMessage;
+import com.kynsof.share.core.domain.response.ErrorField;
 import com.kynsof.share.core.domain.rules.BusinessRule;
 
 public class QualificationDescriptionMustBeNotNullRule extends BusinessRule {
@@ -12,16 +13,19 @@ public class QualificationDescriptionMustBeNotNullRule extends BusinessRule {
     private QualificationDto qualification;
 
     public QualificationDescriptionMustBeNotNullRule(IQualificationService service, QualificationDto qualification) {
-        super(DomainErrorMessage.QUALIFICATION_DESCRIPTION_NOT_NULL, "Qualification description not null!");
+        super(DomainErrorMessage.QUALIFICATION_DESCRIPTION_NOT_NULL,
+                new ErrorField("Description",
+                        "Qualification description not null!")
+        );
         this.service = service;
         this.qualification = qualification;
     }
 
     @Override
     public boolean isBroken() {
-        return null == qualification.getDescription() ||
-                qualification.getDescription().isBlank() || 
-                qualification.getDescription().isEmpty();
+        return null == qualification.getDescription()
+                || qualification.getDescription().isBlank()
+                || qualification.getDescription().isEmpty();
     }
 
 }
--- calendar/src/main/java/com/kynsof/calendar/domain/rules/QualificationDescriptionMustBeUniqueRule.java
+++ calendar/src/main/java/com/kynsof/calendar/domain/rules/QualificationDescriptionMustBeUniqueRule.java
@@ -3,6 +3,7 @@ package com.kynsof.calendar.domain.rules;
 import com.kynsof.calendar.domain.dto.QualificationDto;
 import com.kynsof.calendar.domain.service.IQualificationService;
 import com.kynsof.share.core.domain.exception.DomainErrorMessage;
+import com.kynsof.share.core.domain.response.ErrorField;
 import com.kynsof.share.core.domain.rules.BusinessRule;
 
 public class QualificationDescriptionMustBeUniqueRule extends BusinessRule {
@@ -12,7 +13,9 @@ public class QualificationDescriptionMustBeUniqueRule extends BusinessRule {
     private QualificationDto qualification;
 
     public QualificationDescriptionMustBeUniqueRule(IQualificationService service, QualificationDto qualification) {
-        super(DomainErrorMessage.QUALIFICATION_DESCRIPTION_UNIQUE, "Qualification description unique!");
+        super(DomainErrorMessage.QUALIFICATION_DESCRIPTION_UNIQUE,
+                new ErrorField("Description",
+                        "Qualification description unique!"));
         this.service = service;
         this.qualification = qualification;
     }
