bb3f2d57532d3bf4dfc61ec84a29f2fcb385ea50
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2024-03-29 09:59:06 -0500

    se actualiza el request de business

--- identity/src/main/java/com/kynsof/identity/application/command/business/create/CreateBusinessRequest.java
+++ identity/src/main/java/com/kynsof/identity/application/command/business/create/CreateBusinessRequest.java
@@ -1,13 +1,11 @@
 package com.kynsof.identity.application.command.business.create;
 
-import com.kynsof.share.core.application.FileRequest;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class CreateBusinessRequest {
-
     private String name;
     private String latitude;
     private String longitude;
