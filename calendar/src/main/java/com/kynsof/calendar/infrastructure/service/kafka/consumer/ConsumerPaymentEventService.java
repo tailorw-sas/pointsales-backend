02c188a3bd9eddbe9f3356be7c3de4e92b2bff0c
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2024-03-29 11:00:22 -0500

    se actualiza el request de business

---
+++ calendar/src/main/java/com/kynsof/calendar/infrastructure/service/kafka/consumer/ConsumerPaymentEventService.java
@@ -0,0 +1,27 @@
+package com.kynsof.calendar.infrastructure.service.kafka.consumer;
+
+import com.kynsof.calendar.domain.service.IBusinessService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@Service
+public class ConsumerPaymentEventService {
+
+    @Autowired
+    private IBusinessService service;
+
+    @KafkaListener(topics = "payment", groupId = "payment-calendar")
+    public void consumer(String event) {
+
+        try {
+            String status = event;
+        } catch (Exception ex) {
+            Logger.getLogger(ConsumerPaymentEventService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+}
