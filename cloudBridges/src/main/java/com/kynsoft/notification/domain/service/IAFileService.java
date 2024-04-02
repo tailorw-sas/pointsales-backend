989d588819c52f480a66075f8bcec2d7f82e5e4f
Yannier <70853534+penaescalonayannier@users.noreply.github.com>
2024-04-02 05:22:30 -0400

    adicione un getById a AFile de cloudBridges y un search

--- cloudBridges/src/main/java/com/kynsoft/notification/domain/service/IAFileService.java
+++ cloudBridges/src/main/java/com/kynsoft/notification/domain/service/IAFileService.java
@@ -1,12 +1,17 @@
 package com.kynsoft.notification.domain.service;
 
+import com.kynsof.share.core.domain.request.FilterCriteria;
+import com.kynsof.share.core.domain.response.PaginatedResponse;
 import com.kynsoft.notification.domain.dto.AFileDto;
+import java.util.List;
 
 import java.util.UUID;
+import org.springframework.data.domain.Pageable;
 
 public interface IAFileService {
     void create(AFileDto object);
     void update(AFileDto object);
     void delete(UUID id);
     AFileDto findById(UUID id);
+    PaginatedResponse search(Pageable pageable, List<FilterCriteria> filterCriteria);
 }
--- cloudBridges/src/main/java/com/kynsoft/notification/infrastructure/service/AFileServiceImpl.java
+++ cloudBridges/src/main/java/com/kynsoft/notification/infrastructure/service/AFileServiceImpl.java
@@ -1,16 +1,24 @@
 package com.kynsoft.notification.infrastructure.service;
 
+import com.kynsof.share.core.domain.request.FilterCriteria;
+import com.kynsof.share.core.domain.response.PaginatedResponse;
+import com.kynsof.share.core.infrastructure.specifications.GenericSpecificationsBuilder;
 import com.kynsof.share.utils.ConfigureTimeZone;
+import com.kynsoft.notification.application.query.file.search.FileResponse;
 import com.kynsoft.notification.domain.dto.AFileDto;
 import com.kynsoft.notification.domain.service.IAFileService;
 import com.kynsoft.notification.infrastructure.entity.AFile;
 import com.kynsoft.notification.infrastructure.repository.command.FileWriteDataJPARepository;
 import com.kynsoft.notification.infrastructure.repository.query.FileReadDataJPARepository;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 import java.util.UUID;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 @Service
 public class AFileServiceImpl implements IAFileService {
@@ -44,6 +52,22 @@ public class AFileServiceImpl implements IAFileService {
             return file.get().toAggregate();
         }
         return null;
+    }
+
+    @Override
+    public PaginatedResponse search(Pageable pageable, List<FilterCriteria> filterCriteria) {
+        GenericSpecificationsBuilder<FileResponse> specifications = new GenericSpecificationsBuilder<>(filterCriteria);
+        Page<AFile> data = this.queryRepository.findAll(specifications, pageable);
+        return getPaginatedResponse(data);
+    }
+
+    private PaginatedResponse getPaginatedResponse(Page<AFile> data) {
+        List<FileResponse> files = new ArrayList<>();
+        for (AFile o : data.getContent()) {
+            files.add(new FileResponse(o.toAggregate()));
+        }
+        return new PaginatedResponse(files, data.getTotalPages(), data.getNumberOfElements(),
+                data.getTotalElements(), data.getSize(), data.getNumber());
     }
 
 }
