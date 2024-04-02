59735992cb26105f66937161cb419e1a30ed9c97
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2024-04-02 08:55:09 -0500

    Se arregla el proceso de creación de roles

--- gateway/src/main/java/com/kynsoft/gateway/infrastructure/services/keycloak/RoleService.java
+++ gateway/src/main/java/com/kynsoft/gateway/infrastructure/services/keycloak/RoleService.java
@@ -1,5 +1,7 @@
 package com.kynsoft.gateway.infrastructure.services.keycloak;
 
+import com.kynsof.share.core.domain.exception.UserAlreadyExistsException;
+import com.kynsof.share.core.domain.response.ErrorField;
 import com.kynsoft.gateway.domain.dto.role.RoleRequest;
 import com.kynsoft.gateway.domain.interfaces.IRoleService;
 import com.kynsoft.gateway.infrastructure.services.kafka.producer.ProducerRegisterRoleEventService;
@@ -9,6 +11,7 @@ import org.keycloak.representations.idm.RoleRepresentation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.ws.rs.ClientErrorException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -24,29 +27,34 @@ public class RoleService implements IRoleService {
 
     @Override
     public void createRole(RoleRequest request) {
-        RealmResource realmResource = keycloakProvider.getRealmResource();
+        try {
+            RealmResource realmResource = keycloakProvider.getRealmResource();
 
-        ClientResource clientResource = realmResource.clients().findByClientId(keycloakProvider.getClient_id()).stream()
-                .findFirst()
-                .map(clientRepresentation -> realmResource.clients().get(clientRepresentation.getId()))
-                .orElseThrow(() -> new RuntimeException("Cliente no encontrado"));
+            ClientResource clientResource = realmResource.clients().findByClientId(keycloakProvider.getClient_id()).stream()
+                    .findFirst()
+                    .map(clientRepresentation -> realmResource.clients().get(clientRepresentation.getId()))
+                    .orElseThrow(() -> new RuntimeException("Cliente no encontrado"));
 
 
-        RoleRepresentation role = new RoleRepresentation();
-        role.setName(request.getName());
-        role.setDescription(request.getDescription());
+            RoleRepresentation role = new RoleRepresentation();
+            role.setName(request.getName());
+            role.setDescription(request.getDescription());
 
-        // Agregar atributo de estado
-        Map<String, List<String>> attributes = new HashMap<>();
-        attributes.put("status", List.of("ACTIVE"));
-        role.setAttributes(attributes);
+            // Agregar atributo de estado
+            Map<String, List<String>> attributes = new HashMap<>();
+            attributes.put("status", List.of("ACTIVE"));
+            role.setAttributes(attributes);
 
-        clientResource.roles().create(role);
-        RoleRepresentation createdRole = clientResource.roles().get(request.getName()).toRepresentation();
+            clientResource.roles().create(role);
+            RoleRepresentation createdRole = clientResource.roles().get(request.getName()).toRepresentation();
 
-        this.producerRegisterRoleEventService.create(createdRole.getId(), request.getName(), request.getDescription());
+            this.producerRegisterRoleEventService.create(createdRole.getId(), request.getName(), request.getDescription());
 
-        createdRole.getId();
+            createdRole.getId();
+        }catch (ClientErrorException ex){
+            throw new UserAlreadyExistsException("Role already exists", new ErrorField("name", "Name is already in use"));
+        }
+
     }
 
     @Override
