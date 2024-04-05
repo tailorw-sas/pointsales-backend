b99f23ef7cadec1de184211ae5b11115fe89a55e
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2024-04-05 15:40:58 -0500

    Se actualiza la forma de obtener el servicio de pago

--- identity/src/main/java/com/kynsof/identity/application/command/user/update/UpdateUserSystemRequest.java
+++ identity/src/main/java/com/kynsof/identity/application/command/user/update/UpdateUserSystemRequest.java
@@ -1,7 +1,5 @@
 package com.kynsof.identity.application.command.user.update;
 
-import com.kynsof.identity.domain.dto.UserStatus;
-import com.kynsof.identity.domain.dto.enumType.UserType;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,7 +10,4 @@ public class UpdateUserSystemRequest {
     private String email;
     private String name;
     private String lastName;
-    private UserStatus status;
-    private byte [] image;
-    private UserType userType;
 }
