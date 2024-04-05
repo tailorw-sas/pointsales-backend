ced8c190200a65551ff3cac80a9ec8c37381f567
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2024-04-05 13:04:15 -0500

    Se actualiza la forma de obtener el servicio de pago

--- calendar/src/main/java/com/kynsof/calendar/application/command/receipt/confirmPayment/ConfirmPaymentReceiptCommandHandler.java
+++ calendar/src/main/java/com/kynsof/calendar/application/command/receipt/confirmPayment/ConfirmPaymentReceiptCommandHandler.java
@@ -9,6 +9,8 @@ import com.kynsof.calendar.domain.service.IPatientsService;
 import com.kynsof.calendar.domain.service.IReceiptService;
 import com.kynsof.calendar.domain.service.IScheduleService;
 import com.kynsof.calendar.domain.service.IServiceService;
+import com.kynsof.share.core.application.payment.domain.placeToPlay.response.TransactionsState;
+import com.kynsof.share.core.application.payment.domain.service.IPaymentServiceClient;
 import com.kynsof.share.core.domain.bus.command.ICommandHandler;
 import org.springframework.stereotype.Component;
 
@@ -19,34 +21,36 @@ public class ConfirmPaymentReceiptCommandHandler implements ICommandHandler<Conf
     private final IPatientsService servicePatient;
     private final IScheduleService serviceSchedule;
     private final IServiceService serviceService;
+    private final IPaymentServiceClient paymentServiceClient;
 
     public ConfirmPaymentReceiptCommandHandler(IReceiptService service, IPatientsService servicePatient,
-                                               IScheduleService serviceSchedule, IServiceService serviceService) {
+                                               IScheduleService serviceSchedule, IServiceService serviceService, IPaymentServiceClient paymentServiceClient) {
         this.service = service;
         this.servicePatient = servicePatient;
         this.serviceSchedule = serviceSchedule;
         this.serviceService = serviceService;
+        this.paymentServiceClient = paymentServiceClient;
     }
 
     @Override
     public void handle(ConfirmPaymentReceiptCommand command) {
+        String status = EStatusReceipt.APPROVED.toString();
+        TransactionsState transactionsState = paymentServiceClient.getTransactionsState(Integer.parseInt(command.getRequestId()));
         ReceiptDto _receipt = this.service.findById(command.getReceiptId());
         PatientDto _patient = this.servicePatient.findById(command.getUserId());
         ScheduleDto _schedule = this.serviceSchedule.findById(command.getScheduleId());
         ServiceDto _service = this.serviceService.findById(command.getServiceId());
 
-        _receipt.setAuthorizationCode(command.getAuthorizationCode());
+        _receipt.setAuthorizationCode(transactionsState.getValue().getAuthorization());
         _receipt.setRequestId(command.getRequestId());
-        _receipt.setReference(command.getReference());
+        _receipt.setReference(transactionsState.getValue().getReference());
         _receipt.setSessionId(command.getSessionId());
         _receipt.setIpAddressPayment(command.getIpAddress());
         _receipt.setUserAgentPayment(command.getUserAgent());
-        if(command.getStatus().equals(EStatusReceipt.APPROVED)){
-            //TO DO
-            //Validar el pago
-            //Enviar Correo de confirmacion
-            _receipt.setStatus(command.getStatus());
 
+        if (transactionsState.getValue().getStatus().getStatus().equals(status)
+        && command.getStatus().equals(EStatusReceipt.APPROVED)){
+            command.getStatus().toString();
         }
 
         if(command.getStatus().equals(EStatusReceipt.CANCEL)){
@@ -54,20 +58,32 @@ public class ConfirmPaymentReceiptCommandHandler implements ICommandHandler<Conf
             //Liverar el stock
             //Enviar Correo de cancelado
             _receipt.setStatus(command.getStatus());
-            _schedule.setStock(_schedule.getStock()+1);
+            cleanStock(_schedule);
 
         }
-        if(command.getStatus().equals(EStatusReceipt.REJECTED)){
+        if (transactionsState.getValue().getStatus().getStatus().equals(status)
+                && command.getStatus().equals(EStatusReceipt.APPROVED)){
             //TO DO
             //Validar el estado del pago, si el estado es pendiente de pago o pago hacer el proceso de confirmado ,sino cambiar el estado
-            //Liverar el stock
+            cleanStock(_schedule);
+
             //Enviar Correo de cancelado
             _receipt.setStatus(command.getStatus());
 
+        }
+        if(command.getStatus().equals(EStatusReceipt.PENDING)){
+            //TODO
+
+            _receipt.setStatus(command.getStatus());
         }
 
 
 
         service.update( _receipt);
+    }
+
+    private void cleanStock(ScheduleDto scheduleDto){
+        scheduleDto.setStock(scheduleDto.getStock()+1);
+        serviceSchedule.update(scheduleDto);
     }
 }
