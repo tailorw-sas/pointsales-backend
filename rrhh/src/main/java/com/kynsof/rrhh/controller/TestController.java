2963a9399f5a24fb63cc60f6c1f79a0ca69fa61e
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2024-04-06 15:47:06 -0500

    Se agrega el modulo de recursos humanos

--- rrhh/src/main/java/com/kynsof/rrhh/controller/TestController.java
+++ rrhh/src/main/java/com/kynsof/rrhh/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.kynsof.identity.controller;
+package com.kynsof.rrhh.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
--- rrhh/src/main/java/com/kynsof/rrhh/infrastructure/repository/identity/UserSystem.java
+++ rrhh/src/main/java/com/kynsof/rrhh/infrastructure/repository/identity/UserSystem.java
@@ -5,13 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
 
 import java.io.Serializable;
-import java.time.LocalDateTime;
 import java.util.UUID;
 
 @NoArgsConstructor
@@ -19,47 +14,17 @@ import java.util.UUID;
 @Getter
 @Setter
 @Entity
-//@Audited
-//@EntityListeners(AuditingEntityListener.class)
 @Table(name = "user_system")
 public class UserSystem implements Serializable {
     @Id
     @Column(name = "id")
     private UUID id;
     @Column(unique = true)
-    private String userName;
-    @Column(unique = true)
     private String email;
+    @Column(unique = true)
+    private String identification;
     private String name;
     private String lastName;
-    @Enumerated(EnumType.STRING)
-    private String status;
-    @Enumerated(EnumType.STRING)
-    private String userType;
     private UUID image;
-
-
-    @CreatedBy
-    private String createdBy;
-
-    @LastModifiedBy
-    private String lastModifiedBy;
-
-    @CreatedDate
-    private LocalDateTime createdDate;
-
-    @LastModifiedDate
-    private LocalDateTime lastModifiedDate;
-
-//    public UserSystem(UserSystemDto dto) {
-//        this.id = dto.getId();
-//        this.userName = dto.getUserName();
-//        this.email = dto.getEmail();
-//        this.name = dto.getName();
-//        this.lastName = dto.getLastName();
-//        this.status = dto.getStatus();
-//        this.userType = dto.getUserType() != null ? dto.getUserType() : UserType.UNDEFINED;
-//        this.image = dto.getIdImage() != null ? dto.getIdImage() : null;
-//    }
-
+    private UUID Fingerprint;
 }
