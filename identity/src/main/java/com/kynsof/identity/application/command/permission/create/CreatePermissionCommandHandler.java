555228a18ef57f5a458655dd985b5a4c35e48fcf
Yannier <70853534+penaescalonayannier@users.noreply.github.com>
2024-04-08 16:33:36 -0400

    Adcione validacion antes de obtener el Module

--- identity/src/main/java/com/kynsof/identity/application/command/permission/create/CreatePermissionCommandHandler.java
+++ identity/src/main/java/com/kynsof/identity/application/command/permission/create/CreatePermissionCommandHandler.java
@@ -9,6 +9,7 @@ import com.kynsof.identity.domain.rules.permission.PermissionCodeMustBeNullRule;
 import com.kynsof.identity.domain.rules.permission.PermissionCodeMustBeUniqueRule;
 import com.kynsof.share.core.domain.RulesChecker;
 import com.kynsof.share.core.domain.bus.command.ICommandHandler;
+import com.kynsof.share.core.domain.rules.ValidateObjectNotNullRule;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -27,7 +28,9 @@ public class CreatePermissionCommandHandler implements ICommandHandler<CreatePer
         RulesChecker.checkRule(new PermissionCodeMustBeNullRule(command.getCode()));
         RulesChecker.checkRule(new PermissionCodeMustBeUniqueRule(this.service, command.getCode(), command.getId()));
 
+        RulesChecker.checkRule(new ValidateObjectNotNullRule(command.getIdModule(), "Permission.module.id", "Permission.Module ID cannot be null."));
         ModuleDto module = this.serviceModule.findById(command.getIdModule());
+
         PermissionDto permissionDto = new PermissionDto(command.getId(), command.getCode(), command.getDescription(), module, command.getAction());
         permissionDto.setStatus(PermissionStatusEnm.ACTIVE);
 
