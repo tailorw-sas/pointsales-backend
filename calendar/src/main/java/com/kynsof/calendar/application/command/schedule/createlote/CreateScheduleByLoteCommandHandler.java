cede2cb9cba1593af1594302b7aaadae2a2b44b2
Yannier <70853534+penaescalonayannier@users.noreply.github.com>
2024-04-08 04:55:48 -0400

    fix error

--- calendar/src/main/java/com/kynsof/calendar/application/command/schedule/createlote/CreateScheduleByLoteCommandHandler.java
+++ calendar/src/main/java/com/kynsof/calendar/application/command/schedule/createlote/CreateScheduleByLoteCommandHandler.java
@@ -34,10 +34,24 @@ public class CreateScheduleByLoteCommandHandler implements ICommandHandler<Creat
     }
 
     private List<LocalDate> getBusinessDays(LocalDate startDate, LocalDate endDate) {
-        return startDate.datesUntil(endDate)
+        // Ajustar el endDate si es sábado o domingo
+        if (endDate.getDayOfWeek() == DayOfWeek.SATURDAY) {
+            endDate = endDate.minusDays(1);
+        } else if (endDate.getDayOfWeek() == DayOfWeek.SUNDAY) {
+            endDate = endDate.minusDays(2);
+        }
+
+        List<LocalDate> businessDays = startDate.datesUntil(endDate)
                 .filter(d -> !d.getDayOfWeek().equals(DayOfWeek.SATURDAY))
                 .filter(d -> !d.getDayOfWeek().equals(DayOfWeek.SUNDAY))
                 .collect(Collectors.toList());
-    }
 
+        // Verificar si el último día de la lista es igual al endDate ajustado
+        if (!businessDays.isEmpty() && !businessDays.get(businessDays.size() - 1).equals(endDate)) {
+            // Si no es igual, agregar el endDate ajustado a la lista
+            businessDays.add(endDate);
+        }
+
+        return businessDays;
+    }
 }
