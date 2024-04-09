951956d28e98087b7b9b8833cd7b043b5747a195
Yannier <70853534+penaescalonayannier@users.noreply.github.com>
2024-04-09 06:48:42 -0400

    adicione el getbyid de user en RRHH

--- rrhh/src/main/java/com/kynsof/rrhh/controller/UserSystemController.java
+++ rrhh/src/main/java/com/kynsof/rrhh/controller/UserSystemController.java
@@ -1,7 +1,13 @@
 package com.kynsof.rrhh.controller;
 
 import com.kynsof.rrhh.application.command.registerFingerprint.RegisterFingerprintRequest;
+import com.kynsof.rrhh.application.query.users.getbyid.FindByIdUserSystemsQuery;
+import com.kynsof.rrhh.application.query.users.getbyid.UserSystemsByIdResponse;
+import com.kynsof.share.core.infrastructure.bus.IMediator;
+import java.util.UUID;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,9 +18,24 @@ import reactor.core.publisher.Mono;
 @RequestMapping("/api/users")
 public class UserSystemController {
 
+    private final IMediator mediator;
+
+    public UserSystemController(IMediator mediator) {
+        this.mediator = mediator;
+    }
+
     @PostMapping("register-fingerprint")
     public Mono<ResponseEntity<?>> registerFingerprint(@RequestBody RegisterFingerprintRequest request){
         return Mono.just(ResponseEntity.ok("Keycloak with ADMIN CLIENT ROLE"));
+    }
+    
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<UserSystemsByIdResponse> getById(@PathVariable UUID id) {
+
+        FindByIdUserSystemsQuery query = new FindByIdUserSystemsQuery(id);
+        UserSystemsByIdResponse response = mediator.send(query);
+
+        return ResponseEntity.ok(response);
     }
 
     //TODO
