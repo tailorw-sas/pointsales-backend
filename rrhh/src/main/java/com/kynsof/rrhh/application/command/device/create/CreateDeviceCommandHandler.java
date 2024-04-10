aab6c1858d75d250eec9e5936c3a49467d8aa7ab
Yannier <70853534+penaescalonayannier@users.noreply.github.com>
2024-04-10 05:30:32 -0400

    Adicione regla para validar IP validos

--- rrhh/src/main/java/com/kynsof/rrhh/application/command/device/create/CreateDeviceCommandHandler.java
+++ rrhh/src/main/java/com/kynsof/rrhh/application/command/device/create/CreateDeviceCommandHandler.java
@@ -4,6 +4,7 @@ import com.kynsof.rrhh.doman.dto.BusinessDto;
 import com.kynsof.rrhh.doman.dto.DeviceDto;
 import com.kynsof.rrhh.doman.interfaces.services.IBusinessService;
 import com.kynsof.rrhh.doman.interfaces.services.IDeviceService;
+import com.kynsof.rrhh.doman.rules.device.DeviceIpValidateRule;
 import com.kynsof.share.core.domain.RulesChecker;
 import com.kynsof.share.core.domain.bus.command.ICommandHandler;
 import com.kynsof.share.core.domain.rules.ValidateObjectNotNullRule;
@@ -22,6 +23,7 @@ public class CreateDeviceCommandHandler implements ICommandHandler<CreateDeviceC
 
     @Override
     public void handle(CreateDeviceCommand command) {
+        RulesChecker.checkRule(new DeviceIpValidateRule(command.getIp()));
         RulesChecker.checkRule(new ValidateObjectNotNullRule<>(command.getBusinessId(), "Device.business.id", "Device.business ID cannot be null."));
         BusinessDto business = this.businessService.findById(command.getBusinessId());
 
