6638915d9c8a345b888e8ef6af26db643cbb8252
Yannier <70853534+penaescalonayannier@users.noreply.github.com>
2024-04-10 14:27:32 -0400

    Modifique para que el evento solo se aplique cuando es la primera vez que se crea una relacion entre User Permission and Business.

--- identity/src/main/java/com/kynsof/identity/infrastructure/services/kafka/producer/ProducerCreateUserBusinessEventService.java
+++ identity/src/main/java/com/kynsof/identity/infrastructure/services/kafka/producer/ProducerCreateUserBusinessEventService.java
@@ -3,6 +3,7 @@ package com.kynsof.identity.infrastructure.services.kafka.producer;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kynsof.identity.domain.dto.UserPermissionBusinessDto;
+import com.kynsof.identity.domain.interfaces.service.IUserPermissionBusinessService;
 import com.kynsof.share.core.domain.kafka.entity.UserBusinessKafka;
 import com.kynsof.share.core.domain.kafka.event.CreateEvent;
 import com.kynsof.share.core.domain.kafka.event.EventType;
@@ -16,8 +17,12 @@ import java.util.logging.Logger;
 public class ProducerCreateUserBusinessEventService {
     private final KafkaTemplate<String, String> producer;
 
-    public ProducerCreateUserBusinessEventService(KafkaTemplate<String, String> producer) {
+    private final IUserPermissionBusinessService userPermissionBusinessService;
+
+    public ProducerCreateUserBusinessEventService(KafkaTemplate<String, String> producer,
+                                                  IUserPermissionBusinessService userPermissionBusinessService) {
         this.producer = producer;
+        this.userPermissionBusinessService = userPermissionBusinessService;
     }
 
     public void create(UserPermissionBusinessDto entity) {
@@ -27,10 +32,17 @@ public class ProducerCreateUserBusinessEventService {
                     entity.getUser().getId(), 
                     entity.getBusiness().getId()
             );
-
-            ObjectMapper objectMapper = new ObjectMapper();
-            String json = objectMapper.writeValueAsString(new CreateEvent<>(event, EventType.CREATED));
-            this.producer.send("user-busines", json);
+            Long cant = userPermissionBusinessService.countByUserAndBusiness(event.getIdUser(), event.getIdBusiness());
+            if (cant == 1) {
+                System.err.println("#######################################################");
+                System.err.println("#######################################################");
+                System.err.println("SE EJECUTA UN CREATED");
+                System.err.println("#######################################################");
+                System.err.println("#######################################################");
+                ObjectMapper objectMapper = new ObjectMapper();
+                String json = objectMapper.writeValueAsString(new CreateEvent<>(event, EventType.CREATED));
+                this.producer.send("user-busines", json);
+            }
         } catch (JsonProcessingException ex) {
             Logger.getLogger(ProducerCreateUserBusinessEventService.class.getName()).log(Level.SEVERE, null, ex);
         }
