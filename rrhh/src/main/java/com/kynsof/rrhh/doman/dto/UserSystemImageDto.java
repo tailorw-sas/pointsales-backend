653454be56472270ec3a2de249cd548ab54726a5
Yannier <70853534+penaescalonayannier@users.noreply.github.com>
2024-04-11 05:22:41 -0400

    Creando la clase UserSystemImage

---
+++ rrhh/src/main/java/com/kynsof/rrhh/doman/dto/UserSystemImageDto.java
@@ -0,0 +1,19 @@
+package com.kynsof.rrhh.doman.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserSystemImageDto implements Serializable {
+
+    private UUID id;
+    private byte[] image;
+}
