86fe293516043c0556d6ec443374c3bee756aabb
Yannier <70853534+penaescalonayannier@users.noreply.github.com>
2024-04-11 12:14:33 -0400

    Adicione regla para validar email

--- identity/src/main/java/com/kynsof/identity/application/command/customer/create/CreateCustomerCommandHandler.java
+++ identity/src/main/java/com/kynsof/identity/application/command/customer/create/CreateCustomerCommandHandler.java
@@ -2,6 +2,8 @@ package com.kynsof.identity.application.command.customer.create;
 
 import com.kynsof.identity.domain.dto.CustomerDto;
 import com.kynsof.identity.domain.interfaces.service.ICustomerService;
+import com.kynsof.identity.domain.rules.customer.CustomerEmailValidateRule;
+import com.kynsof.share.core.domain.RulesChecker;
 import com.kynsof.share.core.domain.bus.command.ICommandHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -18,6 +20,8 @@ public class CreateCustomerCommandHandler implements ICommandHandler<CreateCusto
 
     @Override
     public void handle(CreateCustomerCommand command) {
+        RulesChecker.checkRule(new CustomerEmailValidateRule(command.getEmail()));
+
         CustomerDto customerDto = new CustomerDto(
                 command.getId(),
                 command.getFirstName(),
