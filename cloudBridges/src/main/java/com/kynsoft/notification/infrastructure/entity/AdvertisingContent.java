6324f4a47946cba21fbe0feca6a99fa733aa09f6
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2024-04-16 05:51:21 -0500

    Se actualiza el evento de integracion

---
+++ cloudBridges/src/main/java/com/kynsoft/notification/infrastructure/entity/AdvertisingContent.java
@@ -0,0 +1,32 @@
+package com.kynsoft.notification.infrastructure.entity;
+
+
+import com.kynsoft.notification.domain.dto.ContentType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AdvertisingContent {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ContentType type;
+}
