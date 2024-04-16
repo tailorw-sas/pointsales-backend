74c694baa63980194a5dc305fbc25fd6aed886a4
Yannier <70853534+penaescalonayannier@users.noreply.github.com>
2024-04-16 12:07:05 -0400

    Adicione command and query

--- cloudBridges/src/main/java/com/kynsoft/notification/domain/dto/AdvertisingContentDto.java
+++ cloudBridges/src/main/java/com/kynsoft/notification/domain/dto/AdvertisingContentDto.java
@@ -1,6 +1,5 @@
-package com.kynsoft.notification.infrastructure.entity;
+package com.kynsoft.notification.domain.dto;
 
-import com.kynsoft.notification.domain.dto.ContentType;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -14,17 +13,9 @@ import java.util.UUID;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class AdvertisingContent {
-    @Id
+public class AdvertisingContentDto {
     private UUID id;
-
-    @Column(nullable = false)
     private String name;
-
-    @Column(nullable = false)
     private String description;
-
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
     private ContentType type;
 }
