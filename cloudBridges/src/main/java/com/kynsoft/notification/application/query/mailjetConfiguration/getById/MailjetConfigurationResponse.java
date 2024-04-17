7669a7af4517524d57aa68b87a5ba3a3e444019f
Yannier <70853534+penaescalonayannier@users.noreply.github.com>
2024-04-17 11:48:29 -0400

    fix

--- cloudBridges/src/main/java/com/kynsoft/notification/application/query/mailjetConfiguration/getById/MailjetConfigurationResponse.java
+++ cloudBridges/src/main/java/com/kynsoft/notification/application/query/mailjetConfiguration/getById/MailjetConfigurationResponse.java
@@ -16,15 +16,15 @@ public class MailjetConfigurationResponse implements IResponse {
     private UUID id;
     private String mailjetApiKey;
     private String mailjetApiSecret;
-    private String fromEmail;
-    private String fromName;
+    private String email;
+    private String name;
 
     public MailjetConfigurationResponse(MailjetConfigurationDto patients) {
         this.id = patients.getId();
         mailjetApiKey = patients.getMailjetApiKey();
         mailjetApiSecret = patients.getMailjetApiSecret();
-        fromEmail = patients.getFromEmail();
-        fromName = patients.getFromName();
+        email = patients.getFromEmail();
+        name = patients.getFromName();
     }
 
 }
