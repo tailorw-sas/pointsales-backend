9db8c95991e70f9734d0439a06e429babb927670
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2024-04-19 15:53:38 -0500

    Se implementa la funcionalidad de leer los parametros de los reportes

--- cloudBridges/src/main/java/com/kynsoft/notification/controller/ReportController.java
+++ cloudBridges/src/main/java/com/kynsoft/notification/controller/ReportController.java
@@ -4,6 +4,8 @@ import com.kynsof.share.core.infrastructure.bus.IMediator;
 import com.kynsoft.notification.application.command.generateTemplate.GenerateTemplateCommand;
 import com.kynsoft.notification.application.command.generateTemplate.GenerateTemplateMessage;
 import com.kynsoft.notification.application.command.generateTemplate.GenerateTemplateRequest;
+import com.kynsoft.notification.application.query.reportTemplate.GetReportParameterByCodeQuery;
+import com.kynsoft.notification.application.query.reportTemplate.GetReportParameterByCodeResponse;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -30,35 +32,13 @@ public class ReportController {
                 .body(response.getResult());
     }
 
-    @GetMapping(value = "/hello-world1", produces = MediaType.TEXT_PLAIN_VALUE)
-    public ResponseEntity<String> getHelloWorldReport1() {
-//        try {
-//          //  byte[] data = reportService.generatePdfReport();
-//            // Codificar el array de bytes del PDF a un String Base64
-//            String base64Pdf = Base64.getEncoder().encodeToString(data);
-//            return ResponseEntity.ok()
-//                    .contentType(MediaType.TEXT_PLAIN)
-//                    .body(base64Pdf);
-//        } catch (JRException e) {
-//            e.printStackTrace();
-//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-        return null;
-    }
 
-//    @GetMapping("/parameters")
-//    public ResponseEntity<String> getReportParameters() {
-//        try {
-//            String jrxmlUrl = "http://d2cebw6tssfqem.cloudfront.net/cita_2024-04-17_11-38-05.jrxml";
-//            String parametersJson = reportService.getReportParameters(jrxmlUrl);
-//            return ResponseEntity.ok(parametersJson);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            return ResponseEntity.internalServerError().body("Error obtaining report parameters: " + e.getMessage());
-//        }
-//    }
+    @GetMapping("/parameters/{reportCode}")
+    public ResponseEntity<?> getReportParameters(@PathVariable String reportCode) {
+        GetReportParameterByCodeQuery query = new GetReportParameterByCodeQuery(reportCode);
+        GetReportParameterByCodeResponse response = medietor.send(query);
+        return ResponseEntity.ok(response);
+    }
 
 }
 
