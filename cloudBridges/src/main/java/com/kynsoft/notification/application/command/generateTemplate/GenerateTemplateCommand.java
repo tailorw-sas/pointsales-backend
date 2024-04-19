046e3aac18df72a181d8a84d302534c78aa061a9
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2024-04-19 15:29:27 -0500

    Se actualiza el evento de integracion

--- cloudBridges/src/main/java/com/kynsoft/notification/application/command/generateTemplate/GenerateTemplateCommand.java
+++ cloudBridges/src/main/java/com/kynsoft/notification/application/command/generateTemplate/GenerateTemplateCommand.java
@@ -2,30 +2,22 @@ package com.kynsoft.notification.application.command.generateTemplate;
 
 import com.kynsof.share.core.domain.bus.command.ICommand;
 import com.kynsof.share.core.domain.bus.command.ICommandMessage;
-import com.kynsoft.notification.domain.dto.ContentType;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.UUID;
+import java.util.Map;
 
 @Getter
 @Setter
 public class GenerateTemplateCommand implements ICommand {
+    private  byte[] result;
+    private final Map<String, Object> parameters;
+    private final String JasperReportCode;
 
-    private UUID id;
-    private String title;
-    private String description;
-    private ContentType type;
-    private byte[] image;
-    private String link;
+    public GenerateTemplateCommand(Map<String, Object> parameters, String jasperReportCode) {
 
-    public GenerateTemplateCommand(String title, String description, ContentType type, byte[] image, String link) {
-        this.id = UUID.randomUUID();
-        this.title = title;
-        this.description = description;
-        this.type = type;
-        this.image = image;
-        this.link = link;
+        this.parameters = parameters;
+        JasperReportCode = jasperReportCode;
     }
 
 //    public static GenerateTemplateCommand fromRequest(GenerateTemplateRequest request) {
@@ -34,6 +26,6 @@ public class GenerateTemplateCommand implements ICommand {
 
     @Override
     public ICommandMessage getMessage() {
-        return new GenerateTemplateMessage(id);
+        return new GenerateTemplateMessage(result);
     }
 }
--- cloudBridges/src/main/java/com/kynsoft/notification/domain/service/IJasperReportTemplateService.java
+++ cloudBridges/src/main/java/com/kynsoft/notification/domain/service/IJasperReportTemplateService.java
@@ -3,15 +3,16 @@ package com.kynsoft.notification.domain.service;
 import com.kynsof.share.core.domain.request.FilterCriteria;
 import com.kynsof.share.core.domain.response.PaginatedResponse;
 import com.kynsoft.notification.domain.dto.JasperReportTemplateDto;
-import java.util.List;
-
-import java.util.UUID;
 import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.UUID;
 
 public interface IJasperReportTemplateService {
     void create(JasperReportTemplateDto object);
     void update(JasperReportTemplateDto object);
     void delete(UUID id);
-    JasperReportTemplateDto findById(UUID id);
     PaginatedResponse search(Pageable pageable, List<FilterCriteria> filterCriteria);
+    JasperReportTemplateDto findById(UUID id);
+    JasperReportTemplateDto findByTemplateCode(String templateCode);
 }
