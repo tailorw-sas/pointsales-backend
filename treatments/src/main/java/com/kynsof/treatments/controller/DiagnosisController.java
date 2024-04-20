fa041cfd0e144272d658a23f8974b36a49425cbb
Yannier <70853534+penaescalonayannier@users.noreply.github.com>
2024-04-20 10:22:11 -0400

    Adicione el search de Diagnosis

--- treatments/src/main/java/com/kynsof/treatments/controller/DiagnosisController.java
+++ treatments/src/main/java/com/kynsof/treatments/controller/DiagnosisController.java
@@ -1,9 +1,14 @@
 package com.kynsof.treatments.controller;
 
+import com.kynsof.share.core.domain.request.SearchRequest;
+import com.kynsof.share.core.domain.response.PaginatedResponse;
 import com.kynsof.share.core.infrastructure.bus.IMediator;
 import com.kynsof.treatments.application.command.diagnosis.create.CreateDiagnosisCommand;
 import com.kynsof.treatments.application.command.diagnosis.create.CreateDiagnosisMessage;
 import com.kynsof.treatments.application.command.diagnosis.create.CreateDiagnosisRequest;
+import com.kynsof.treatments.application.query.diagnosis.search.GetSearchDiagnosisQuery;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,6 +29,15 @@ public class DiagnosisController {
         CreateDiagnosisMessage response = mediator.send(createCommand);
 
         return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/search")
+    public ResponseEntity<PaginatedResponse> search(@RequestBody SearchRequest request)
+    {
+        Pageable pageable = PageRequest.of(request.getPage(), request.getPageSize());
+        GetSearchDiagnosisQuery query = new GetSearchDiagnosisQuery(pageable, request.getFilter(),request.getQuery());
+        PaginatedResponse data = mediator.send(query);
+        return ResponseEntity.ok(data);
     }
 
 }
