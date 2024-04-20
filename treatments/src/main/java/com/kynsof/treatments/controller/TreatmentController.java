42c5d5b883a599db5311c84d105fadd517c70fcf
Yannier <70853534+penaescalonayannier@users.noreply.github.com>
2024-04-20 08:23:05 -0400

    Adicione el search de tratamiento

--- treatments/src/main/java/com/kynsof/treatments/controller/TreatmentController.java
+++ treatments/src/main/java/com/kynsof/treatments/controller/TreatmentController.java
@@ -1,12 +1,17 @@
 package com.kynsof.treatments.controller;
 
+import com.kynsof.share.core.domain.request.SearchRequest;
+import com.kynsof.share.core.domain.response.PaginatedResponse;
 import com.kynsof.share.core.infrastructure.bus.IMediator;
 import com.kynsof.treatments.application.command.treatment.create.CreateTreatmentCommand;
 import com.kynsof.treatments.application.command.treatment.create.CreateTreatmentMessage;
 import com.kynsof.treatments.application.command.treatment.create.CreateTreatmentRequest;
 import com.kynsof.treatments.application.query.treatment.getbyid.FindByIdTreatmentQuery;
 import com.kynsof.treatments.application.query.treatment.getbyid.TreatmentResponse;
+import com.kynsof.treatments.application.query.treatment.search.GetSearchTreatmentQuery;
 import java.util.UUID;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -36,6 +41,15 @@ public class TreatmentController {
         TreatmentResponse response = mediator.send(query);
 
         return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/search")
+    public ResponseEntity<PaginatedResponse> search(@RequestBody SearchRequest request)
+    {
+        Pageable pageable = PageRequest.of(request.getPage(), request.getPageSize());
+        GetSearchTreatmentQuery query = new GetSearchTreatmentQuery(pageable, request.getFilter(),request.getQuery());
+        PaginatedResponse data = mediator.send(query);
+        return ResponseEntity.ok(data);
     }
 
 }
