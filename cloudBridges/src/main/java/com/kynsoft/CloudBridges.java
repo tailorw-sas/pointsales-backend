7f5693e6ef6f1000b671264a73a787b28c4c31bb
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2024-04-20 14:47:27 -0500

    Se implementa la funcionalidad de leer los parametros de los reportes

--- cloudBridges/src/main/java/com/kynsoft/CloudBridges.java
+++ cloudBridges/src/main/java/com/kynsoft/CloudBridges.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CloudBridges {
     public static void main(String[] args) {
+        System.setProperty("java.awt.headless", "true");
         SpringApplication.run(CloudBridges.class, args);
 
     }
