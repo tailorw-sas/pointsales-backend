67353f57b7af681237dd79a710a36f00aa920b2c
Yannier <70853534+penaescalonayannier@users.noreply.github.com>
2024-04-20 08:18:46 -0400

    Adicione el getbyid de tratamiento

--- treatments/src/main/java/com/kynsof/treatments/controller/TreatmentController.java
+++ treatments/src/main/java/com/kynsof/treatments/controller/TreatmentController.java
@@ -4,6 +4,9 @@ import com.kynsof.share.core.infrastructure.bus.IMediator;
 import com.kynsof.treatments.application.command.treatment.create.CreateTreatmentCommand;
 import com.kynsof.treatments.application.command.treatment.create.CreateTreatmentMessage;
 import com.kynsof.treatments.application.command.treatment.create.CreateTreatmentRequest;
+import com.kynsof.treatments.application.query.treatment.getbyid.FindByIdTreatmentQuery;
+import com.kynsof.treatments.application.query.treatment.getbyid.TreatmentResponse;
+import java.util.UUID;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,4 +28,14 @@ public class TreatmentController {
 
         return ResponseEntity.ok(response);
     }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<TreatmentResponse> getById(@PathVariable UUID id) {
+
+        FindByIdTreatmentQuery query = new FindByIdTreatmentQuery(id);
+        TreatmentResponse response = mediator.send(query);
+
+        return ResponseEntity.ok(response);
+    }
+
 }
