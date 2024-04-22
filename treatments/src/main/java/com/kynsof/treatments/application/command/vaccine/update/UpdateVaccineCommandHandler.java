ecd512d2e67b4f425a59ce2a9e67c7479d7097cf
Yannier <70853534+penaescalonayannier@users.noreply.github.com>
2024-04-22 06:41:02 -0400

    Adicione validacion al update de vaccine

--- treatments/src/main/java/com/kynsof/treatments/application/command/vaccine/update/UpdateVaccineCommandHandler.java
+++ treatments/src/main/java/com/kynsof/treatments/application/command/vaccine/update/UpdateVaccineCommandHandler.java
@@ -1,7 +1,11 @@
 package com.kynsof.treatments.application.command.vaccine.update;
 
+import com.kynsof.share.core.domain.RulesChecker;
 import com.kynsof.share.core.domain.bus.command.ICommandHandler;
+import com.kynsof.share.core.domain.rules.ValidateObjectNotNullRule;
+import com.kynsof.share.utils.UpdateIfNotNull;
 import com.kynsof.treatments.domain.dto.VaccineDto;
+import com.kynsof.treatments.domain.rules.vaccine.VaccineNameMustBeUniqueRule;
 import com.kynsof.treatments.domain.service.IVaccineService;
 import org.springframework.stereotype.Component;
 
@@ -16,20 +20,22 @@ public class UpdateVaccineCommandHandler implements ICommandHandler<UpdateVaccin
 
     @Override
     public void handle(UpdateVaccineCommand command) {
+        RulesChecker.checkRule(new ValidateObjectNotNullRule<>(command.getId(), "id", "Vaccine ID cannot be null."));
         VaccineDto _vaccine = this.serviceImpl.findById(command.getId());
 
-        //TODO Yannier realizar las reglas de validacion
+        if (UpdateIfNotNull.updateIfStringNotNull(_vaccine::setName, command.getName())) {
+            RulesChecker.checkRule(new VaccineNameMustBeUniqueRule(this.serviceImpl, command.getName(), command.getId()));
+        }
+        UpdateIfNotNull.updateIfStringNotNull(_vaccine::setDescription, command.getDescription());
+        UpdateIfNotNull.updateIfStringNotNull(_vaccine::setDose, command.getDose());
 
-        _vaccine.setName(command.getName());
-        _vaccine.setDescription(command.getDescription());
         _vaccine.setType(command.getType());
         _vaccine.setMinAge(command.getMinAge());
         _vaccine.setMaxAge(command.getMaxAge());
-        _vaccine.setDose(command.getDose());
         _vaccine.setRouteOfAdministration(command.getRouteOfAdministration());
         _vaccine.setPreventableDiseases(command.getPreventableDiseases());
         _vaccine.setServiceId(command.getServiceId());
-        serviceImpl.update(_vaccine);
 
+        serviceImpl.update(_vaccine);
     }
 }
