f03233c0a29613e287521cf77e93ab9bfb52655c
Yannier <70853534+penaescalonayannier@users.noreply.github.com>
2024-04-24 08:22:36 -0400

    Adicione evento para enviar a tratamiento la info del contacto

--- patients/src/main/java/com/kynsof/patients/application/command/contactInfo/create/CreateContactInfoCommandHandler.java
+++ patients/src/main/java/com/kynsof/patients/application/command/contactInfo/create/CreateContactInfoCommandHandler.java
@@ -7,6 +7,7 @@ import com.kynsof.patients.domain.dto.enumTye.Status;
 import com.kynsof.patients.domain.service.IContactInfoService;
 import com.kynsof.patients.domain.service.IGeographicLocationService;
 import com.kynsof.patients.domain.service.IPatientsService;
+import com.kynsof.patients.infrastructure.services.kafka.producer.ProducerCreateContactEventService;
 import com.kynsof.share.core.domain.bus.command.ICommandHandler;
 import org.springframework.stereotype.Component;
 
@@ -18,11 +19,14 @@ public class CreateContactInfoCommandHandler implements ICommandHandler<CreateCo
     private final IContactInfoService contactInfoService;
     private final IPatientsService patientsService;
     private final IGeographicLocationService geographicLocationService;
+    private final ProducerCreateContactEventService producerCreateContactEventService;
 
-    public CreateContactInfoCommandHandler(IContactInfoService serviceImpl, IPatientsService patientsService, IGeographicLocationService geographicLocationService) {
+    public CreateContactInfoCommandHandler(IContactInfoService serviceImpl, IPatientsService patientsService, 
+            IGeographicLocationService geographicLocationService, ProducerCreateContactEventService producerCreateContactEventService) {
         this.contactInfoService = serviceImpl;
         this.patientsService = patientsService;
         this.geographicLocationService = geographicLocationService;
+        this.producerCreateContactEventService = producerCreateContactEventService;
     }
 
     @Override
@@ -30,28 +34,31 @@ public class CreateContactInfoCommandHandler implements ICommandHandler<CreateCo
         PatientDto patientDto = patientsService.findByIdSimple(command.getPatientId());
         GeographicLocationDto geographicLocationDto = geographicLocationService.findById(command.getGeographicLocationId());
 
-      ContactInfoDto contactInfoDto =  contactInfoService.findByPatientId(command.getPatientId());
+        ContactInfoDto contactInfoDto = contactInfoService.findByPatientId(command.getPatientId());
 
-      if(contactInfoDto == null) {
-          UUID id = contactInfoService.create(new ContactInfoDto(
-                  UUID.randomUUID(),
-                  patientDto,
-                  "",
-                  command.getTelephone(),
-                  command.getAddress(),
-                  command.getBirthdayDate(),
-                  Status.ACTIVE,
-                  geographicLocationDto
-          ));
-          command.setId(id);
-      }else {
-          contactInfoDto.setAddress(command.getAddress());
-          contactInfoDto.setBirthdayDate(command.getBirthdayDate());
-          contactInfoDto.setTelephone(command.getTelephone());
-          contactInfoDto.setStatus(Status.ACTIVE);
-          contactInfoService.update(contactInfoDto);
-          command.setId(contactInfoDto.getId());
-      }
-    //TODO yannier Evento de confirmación de creacion de usuario y datos de como debe acceder, se envia un correo
+        if (contactInfoDto == null) {
+            ContactInfoDto create = new ContactInfoDto(
+                    UUID.randomUUID(),
+                    patientDto,
+                    "",
+                    command.getTelephone(),
+                    command.getAddress(),
+                    command.getBirthdayDate(),
+                    Status.ACTIVE,
+                    geographicLocationDto
+            );
+            UUID id = contactInfoService.create(create);
+            command.setId(id);
+            this.producerCreateContactEventService.create(create);
+        } else {
+            contactInfoDto.setAddress(command.getAddress());
+            contactInfoDto.setBirthdayDate(command.getBirthdayDate());
+            contactInfoDto.setTelephone(command.getTelephone());
+            contactInfoDto.setStatus(Status.ACTIVE);
+            contactInfoService.update(contactInfoDto);
+            command.setId(contactInfoDto.getId());
+            this.producerCreateContactEventService.create(contactInfoDto);
+        }
+        //TODO yannier Evento de confirmación de creacion de usuario y datos de como debe acceder, se envia un correo
     }
 }
