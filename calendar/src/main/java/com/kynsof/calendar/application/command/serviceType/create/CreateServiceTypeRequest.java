bb81d2e424c801544d7cdad774131aa0f3b1565b
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2024-04-29 12:06:18 -0500

    se actualiza el controller de medicamento

--- calendar/src/main/java/com/kynsof/calendar/application/command/serviceType/create/CreateServiceTypeRequest.java
+++ calendar/src/main/java/com/kynsof/calendar/application/command/serviceType/create/CreateServiceTypeRequest.java
@@ -1,5 +1,6 @@
 package com.kynsof.calendar.application.command.serviceType.create;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,6 +9,7 @@ import lombok.Setter;
 public class CreateServiceTypeRequest {
 
     private String name;
+    @NotBlank(message = "Image is required and cannot be blank")
     private  String image;
 
 }
