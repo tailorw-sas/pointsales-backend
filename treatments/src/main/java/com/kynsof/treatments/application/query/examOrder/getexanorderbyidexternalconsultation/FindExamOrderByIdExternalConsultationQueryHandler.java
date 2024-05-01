d30f9a64c305091c9a834fff807ac83c1dd290fc
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2024-05-01 11:30:04 -0500

    Se implementa el filtro en el crud de la entidad service de calendar

--- treatments/src/main/java/com/kynsof/treatments/application/query/examOrder/getexanorderbyidexternalconsultation/FindExamOrderByIdExternalConsultationQueryHandler.java
+++ treatments/src/main/java/com/kynsof/treatments/application/query/examOrder/getexanorderbyidexternalconsultation/FindExamOrderByIdExternalConsultationQueryHandler.java
@@ -2,7 +2,9 @@ package com.kynsof.treatments.application.query.examOrder.getexanorderbyidextern
 
 import com.kynsof.share.core.domain.bus.query.IQueryHandler;
 import com.kynsof.treatments.application.query.examOrder.getall.ExamOrderResponse;
+import com.kynsof.treatments.domain.dto.ExamOrderDto;
 import com.kynsof.treatments.domain.dto.ExternalConsultationDto;
+import com.kynsof.treatments.domain.service.IExamOrderService;
 import com.kynsof.treatments.domain.service.IExternalConsultationService;
 import org.springframework.stereotype.Component;
 
@@ -10,15 +12,17 @@ import org.springframework.stereotype.Component;
 public class FindExamOrderByIdExternalConsultationQueryHandler implements IQueryHandler<FindExamOrderByIdExternalConsultationQuery, ExamOrderResponse> {
 
     private final IExternalConsultationService externalConsultationService;
-
-    public FindExamOrderByIdExternalConsultationQueryHandler(IExternalConsultationService externalConsultationService) {
-        this.externalConsultationService = externalConsultationService;
+    private final IExamOrderService examOrderService;
+    public FindExamOrderByIdExternalConsultationQueryHandler(IExternalConsultationService externalConsultationService1, IExamOrderService externalConsultationService) {
+        this.externalConsultationService = externalConsultationService1;
+        this.examOrderService = externalConsultationService;
     }
 
     @Override
     public ExamOrderResponse handle(FindExamOrderByIdExternalConsultationQuery query) {
-        ExternalConsultationDto externalConsultationDt = this.externalConsultationService.findById(query.getId());
+        ExternalConsultationDto externalConsultationDto = externalConsultationService.findById(query.getId());
+        ExamOrderDto examOrderDto = this.examOrderService.findByExternalConsultation(externalConsultationDto);
 
-      return new ExamOrderResponse(externalConsultationDt.getExamOrder());
+      return new ExamOrderResponse(examOrderDto);
     }
 }
