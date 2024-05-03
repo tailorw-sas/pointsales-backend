18a755f7b9c4ece0b31dd6e59b2fea781fa00cde
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2024-05-03 08:42:42 -0500

    Se implementa el filtro en el crud de la entidad service de calendar

--- config/src/main/java/com/kynsof/configcloudservice/ConfigCloudApplication.java
+++ config/src/main/java/com/kynsof/configcloudservice/ConfigCloudApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class ConfigCloudServiceApplication {
+public class ConfigCloudApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfigCloudServiceApplication.class, args);
+        SpringApplication.run(ConfigCloudApplication.class, args);
     }
 
 }
