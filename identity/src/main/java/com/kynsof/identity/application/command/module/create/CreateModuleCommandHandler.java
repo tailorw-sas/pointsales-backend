ec5796239d2cfac11f4631ebb6bf73bcc729a973
Yannier <70853534+penaescalonayannier@users.noreply.github.com>
2024-05-09 05:15:10 -0400

    refactor

--- identity/src/main/java/com/kynsof/identity/application/command/module/create/CreateModuleCommandHandler.java
+++ identity/src/main/java/com/kynsof/identity/application/command/module/create/CreateModuleCommandHandler.java
@@ -7,19 +7,12 @@ import com.kynsof.identity.domain.rules.module.ModuleNameMustBeNullRule;
 import com.kynsof.identity.domain.rules.module.ModuleNameMustBeUniqueRule;
 import com.kynsof.share.core.domain.RulesChecker;
 import com.kynsof.share.core.domain.bus.command.ICommandHandler;
-import com.kynsof.share.core.domain.kafka.producer.s3.ProducerSaveFileEventService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import java.util.UUID;
 
 @Component
 public class CreateModuleCommandHandler implements ICommandHandler<CreateModuleCommand> {
 
     private final IModuleService service;
-
-    @Autowired
-    private ProducerSaveFileEventService saveFileEventService;
 
     public CreateModuleCommandHandler(IModuleService service) {
         this.service = service;
@@ -31,15 +24,11 @@ public class CreateModuleCommandHandler implements ICommandHandler<CreateModuleC
         RulesChecker.checkRule(new ModuleDescriptionMustBeNullRule(command.getDescription()));
         RulesChecker.checkRule(new ModuleNameMustBeUniqueRule(this.service, command.getName(), command.getId()));
 
-        UUID idImage = UUID.randomUUID();
         service.create(new ModuleDto(
                 command.getId(),
                 command.getName(),
                 command.getImage(),
                 command.getDescription())
         );
-//        FileKafka fileSave = new FileKafka(idImage, "identity", UUID.randomUUID().toString(),
-//                command.getImage());
-      //  saveFileEventService.create(fileSave);
     }
 }
