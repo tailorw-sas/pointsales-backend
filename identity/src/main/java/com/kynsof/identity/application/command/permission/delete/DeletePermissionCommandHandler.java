cba06720c481c43355825edf8db532a2f45c297f
Yannier <70853534+penaescalonayannier@users.noreply.github.com>
2024-05-09 15:42:46 -0400

    fix

--- identity/src/main/java/com/kynsof/identity/application/command/permission/delete/DeletePermissionCommandHandler.java
+++ identity/src/main/java/com/kynsof/identity/application/command/permission/delete/DeletePermissionCommandHandler.java
@@ -4,6 +4,7 @@ import com.kynsof.identity.domain.dto.PermissionDto;
 import com.kynsof.identity.domain.dto.enumType.PermissionStatusEnm;
 import com.kynsof.identity.domain.interfaces.service.IPermissionService;
 import com.kynsof.share.core.domain.bus.command.ICommandHandler;
+import java.util.UUID;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -20,6 +21,7 @@ public class DeletePermissionCommandHandler implements ICommandHandler<DeletePer
         PermissionDto delete = this.service.findById(command.getId());
 
         delete.setStatus(PermissionStatusEnm.INACTIVE);
+        delete.setCode(delete.getCode() + "-" + UUID.randomUUID());
         delete.setDeleted(true);
 
         service.delete(delete);
