2961922a3253baab57cd6bbaa27cbf562c0a0aae
Yannier <70853534+penaescalonayannier@users.noreply.github.com>
2024-05-09 07:22:59 -0400

    refactor

--- identity/src/main/java/com/kynsof/identity/application/command/module/update/UpdateModuleCommandHandler.java
+++ identity/src/main/java/com/kynsof/identity/application/command/module/update/UpdateModuleCommandHandler.java
@@ -5,24 +5,14 @@ import com.kynsof.identity.domain.interfaces.service.IModuleService;
 import com.kynsof.identity.domain.rules.module.ModuleNameMustBeUniqueRule;
 import com.kynsof.share.core.domain.RulesChecker;
 import com.kynsof.share.core.domain.bus.command.ICommandHandler;
-import com.kynsof.share.core.domain.kafka.producer.s3.ProducerDeleteFileEventService;
-import com.kynsof.share.core.domain.kafka.producer.s3.ProducerSaveFileEventService;
 import com.kynsof.share.core.domain.rules.ValidateObjectNotNullRule;
 import com.kynsof.share.utils.UpdateIfNotNull;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 
 @Component
 public class UpdateModuleCommandHandler implements ICommandHandler<UpdateModuleCommand> {
 
     private final IModuleService service;
-
-    @Autowired
-    private ProducerSaveFileEventService saveFileEventService;
-
-    @Autowired
-    private ProducerDeleteFileEventService deleteFileEventService;
 
     public UpdateModuleCommandHandler(IModuleService service) {
         this.service = service;
@@ -35,16 +25,14 @@ public class UpdateModuleCommandHandler implements ICommandHandler<UpdateModuleC
 
         ModuleDto module = this.service.findById(command.getId());
 
-        UpdateIfNotNull.updateIfNotNull(module::setDescription, command.getDescription());
+        UpdateIfNotNull.updateIfStringNotNull(module::setDescription, command.getDescription());
 
-        if (!StringUtils.isEmpty(command.getName())) {
+        if (UpdateIfNotNull.updateIfStringNotNull(module::setName, command.getName())) {
             RulesChecker.checkRule(new ModuleNameMustBeUniqueRule(this.service, command.getName(), command.getId()));
-            module.setName(command.getName());
         }
         UpdateIfNotNull.updateIfStringNotNull(module::setImage, command.getImage());
 
         this.service.update(module);
-
 
     }
 }
