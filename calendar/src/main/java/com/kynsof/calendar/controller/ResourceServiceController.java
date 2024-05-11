434ba69192e20db1a93e0e146ebac058eeff579b
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2024-05-11 13:36:17 -0500

    Update Service url

---
+++ calendar/src/main/java/com/kynsof/calendar/controller/ResourceServiceController.java
@@ -0,0 +1,33 @@
+package com.kynsof.calendar.controller;
+
+import com.kynsof.calendar.application.command.resource.addServices.AddServiceCommand;
+import com.kynsof.calendar.application.command.resource.addServices.AddServiceMessage;
+import com.kynsof.calendar.application.command.resource.addServices.AddServiceRequest;
+import com.kynsof.share.core.infrastructure.bus.IMediator;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/resource-services")
+public class ResourceServiceController {
+
+    private final IMediator mediator;
+
+    public ResourceServiceController(IMediator mediator){
+
+        this.mediator = mediator;
+    }
+
+
+    @PostMapping("")
+    public ResponseEntity<?> addServices( @RequestBody AddServiceRequest request)
+    {
+        AddServiceCommand command = AddServiceCommand.fromRequest(request);
+        AddServiceMessage response = mediator.send(command);
+        return ResponseEntity.ok(response);
+    }
+
+}
