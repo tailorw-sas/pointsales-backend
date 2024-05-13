756f1d026d128999530e8c36e653d09d84dd93ac
Yannier <70853534+penaescalonayannier@users.noreply.github.com>
2024-05-13 14:49:26 -0400

    fix

--- identity/src/main/java/com/kynsof/identity/IdentityApplication.java
+++ identity/src/main/java/com/kynsof/identity/IdentityApplication.java
@@ -6,34 +6,6 @@ import org.springframework.cache.annotation.EnableCaching;
 
 @EnableCaching
 @SpringBootApplication
-//public class IdentityApplication implements CommandLineRunner {
-//
-////    @Autowired
-////    private ModuleServiceImpl moduleServiceImpl;
-////
-////    @Autowired
-////    private CustomerServiceImpl customerServiceImpl;
-////
-////    @Autowired
-////    private UserSystemServiceImpl userServiceImpl;
-//
-//    public static void main(String[] args) {
-//        SpringApplication.run(IdentityApplication.class, args);
-//    }
-//
-//    @Override
-//    public void run(String... args) throws Exception {
-//
-//    }
-//
-////    @Override
-////    public void run(String... args) throws Exception {
-////        this.moduleServiceImpl.updateDelete();
-////        this.customerServiceImpl.updateDelete();
-////        this.userServiceImpl.updateDelete();
-////    }
-//
-//}
 public class IdentityApplication {
     public static void main(String[] args) {
         SpringApplication.run(IdentityApplication.class, args);
