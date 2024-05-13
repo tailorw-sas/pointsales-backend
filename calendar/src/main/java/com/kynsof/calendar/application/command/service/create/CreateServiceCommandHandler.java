309878a20c6ea906180ec94ecf5cb4fef5236a54
Yannier <70853534+penaescalonayannier@users.noreply.github.com>
2024-05-13 12:29:49 -0400

    corregi para que el UUID que devuelve el comando del crear services sea el correcto

--- calendar/src/main/java/com/kynsof/calendar/application/command/service/create/CreateServiceCommandHandler.java
+++ calendar/src/main/java/com/kynsof/calendar/application/command/service/create/CreateServiceCommandHandler.java
@@ -8,20 +8,16 @@ import com.kynsof.calendar.domain.service.IServiceService;
 import com.kynsof.calendar.domain.service.IServiceTypeService;
 import com.kynsof.share.core.domain.RulesChecker;
 import com.kynsof.share.core.domain.bus.command.ICommandHandler;
-import com.kynsof.share.core.domain.kafka.producer.s3.ProducerSaveFileEventService;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CreateServiceCommandHandler implements ICommandHandler<CreateServiceCommand> {
 
     private final IServiceService service;
-    private final ProducerSaveFileEventService saveFileEventService;
     private final IServiceTypeService serviceTypeService;
 
-
-    public CreateServiceCommandHandler(IServiceService service, ProducerSaveFileEventService saveFileEventService, IServiceTypeService serviceTypeService) {
+    public CreateServiceCommandHandler(IServiceService service, IServiceTypeService serviceTypeService) {
         this.service = service;
-        this.saveFileEventService = saveFileEventService;
         this.serviceTypeService = serviceTypeService;
     }
 
@@ -30,16 +26,17 @@ public class CreateServiceCommandHandler implements ICommandHandler<CreateServic
         RulesChecker.checkRule(new SeviceNameMustBeUniqueRule(this.service, command.getName(), command.getId()));
         ServiceTypeDto serviceTypeDto = serviceTypeService.findById(command.getType());
 
-
-        service.create(new ServiceDto(
-                command.getId(), 
+        ServiceDto serviceDto = service.create(new ServiceDto(
+                command.getId(),
                 serviceTypeDto,
                 EServiceStatus.ACTIVE,
                 command.getImage(),
-                command.getName(), 
-                command.getNormalAppointmentPrice(), 
-                command.getExpressAppointmentPrice(), 
+                command.getName(),
+                command.getNormalAppointmentPrice(),
+                command.getExpressAppointmentPrice(),
                 command.getDescription(),
-                command.isApplyIva() ));
+                command.isApplyIva()));
+
+        command.setId(serviceDto.getId());
     }
 }
