b1d1c2a6a5ea855d2e1b04f97ee4a7a31dca3d15
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2024-05-14 18:44:38 -0500

    Add selected Business

--- identity/src/main/java/com/kynsof/identity/application/command/user/changeSelectedBusiness/ChangeSelectedBusinessRequest.java
+++ identity/src/main/java/com/kynsof/identity/application/command/user/changeSelectedBusiness/ChangeSelectedBusinessRequest.java
@@ -1,6 +1,5 @@
 package com.kynsof.identity.application.command.user.changeSelectedBusiness;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,7 +7,6 @@ import java.util.UUID;
 
 @Getter
 @Setter
-@AllArgsConstructor
 public class ChangeSelectedBusinessRequest {
     private UUID businessId;
 }
