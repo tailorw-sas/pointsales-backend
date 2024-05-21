3817a79c9c4d1a249818043270311a55e63943ae
Yannier <70853534+penaescalonayannier@users.noreply.github.com>
2024-05-21 15:36:41 -0400

    Adicione funcion validateEqualsDate

--- share/src/main/java/com/kynsof/share/utils/ConfigureTimeZone.java
+++ share/src/main/java/com/kynsof/share/utils/ConfigureTimeZone.java
@@ -1,6 +1,7 @@
 package com.kynsof.share.utils;
 
 import java.time.Instant;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
@@ -16,13 +17,20 @@ public class ConfigureTimeZone {
         
         return localDateTime;
     }
-    
+
     public static LocalDateTime convertDateToLocalDateTime(Date date) {
         Instant instant = date.toInstant();
-        ZonedDateTime zonedDateTime = instant.atZone(ZoneId.systemDefault());
+        ZonedDateTime zonedDateTime = instant.atZone(ZoneId.of(ZONE_ID));
         LocalDateTime localDateTime = zonedDateTime.toLocalDateTime();
 
         return localDateTime;
     }
 
+    public static boolean validateEqualsDate(LocalDateTime validate) {
+        LocalDateTime today = ConfigureTimeZone.getTimeZone();
+        LocalDate actual = today.toLocalDate();
+        LocalDate save = validate.toLocalDate();
+
+        return !save.equals(actual);
+    }
 }
