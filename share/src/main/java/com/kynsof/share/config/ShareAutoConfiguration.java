ef8448dc16cc7bda2adb298487ad2d984bb4d08f
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2024-05-25 17:01:18 -0500

    Se implementa servicio para notificar al usuario la cita

--- share/src/main/java/com/kynsof/share/config/ShareAutoConfiguration.java
+++ share/src/main/java/com/kynsof/share/config/ShareAutoConfiguration.java
@@ -22,10 +22,8 @@ public class ShareAutoConfiguration {
 
     @Value("${rc.microservice.feign.connect-timeout:300000}")
     private Long feignConnectTimeout;
-
     @Value("${rc.microservice.feign.read-timeout:300000}")
     private Long feignReadTimeout;
-
     @Bean
     @Scope("prototype")
     public Feign.Builder feignBuilder() {
