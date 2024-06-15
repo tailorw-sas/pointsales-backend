5019b514591a34e2160b19a9a4ffda6bfdffe74b
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2024-06-15 05:20:55 -0500

    Fix error

--- calendar/src/main/java/com/kynsof/calendar/application/command/schedule/update/UpdateScheduleCommandHandler.java
+++ calendar/src/main/java/com/kynsof/calendar/application/command/schedule/update/UpdateScheduleCommandHandler.java
@@ -1,51 +1,25 @@
 package com.kynsof.calendar.application.command.schedule.update;
 
-import com.kynsof.calendar.application.command.schedule.create.CreateScheduleCommand;
-import com.kynsof.calendar.domain.dto.ReceiptDto;
-import com.kynsof.calendar.domain.service.IReceiptService;
+import com.kynsof.calendar.domain.dto.ScheduleDto;
+import com.kynsof.calendar.domain.service.IScheduleService;
 import com.kynsof.share.core.domain.bus.command.ICommandHandler;
-import com.kynsof.share.core.infrastructure.bus.IMediator;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UpdateScheduleCommandHandler implements ICommandHandler<UpdateScheduleCommand> {
 
-    private final IReceiptService serviceReceipt;
-    private IMediator mediator;
+    private final IScheduleService scheduleService;
 
-    public UpdateScheduleCommandHandler(IReceiptService serviceReceipt) {
-        this.serviceReceipt = serviceReceipt;
+    public UpdateScheduleCommandHandler( IScheduleService scheduleService) {
+        this.scheduleService = scheduleService;
     }
 
     @Override
     public void handle(UpdateScheduleCommand command) {
-        this.mediator = command.getMediator();
-        if (command.getUser() != null) {
-            /**
-             * Se busca si existe una reserva para el usuario y con el mismo
-             * horario.
-             */
-            ReceiptDto _receipt = this.serviceReceipt.findReceiptByUserIdAndScheduleId(command.getUser(), command.getId());
-
-            if (_receipt == null) {
-                /**
-                 * Si no existe la reserva se manda a crear.
-                 */
-                this.mediator.send(new CreateScheduleCommand(
-                        command.getResource(),
-                        command.getBusiness(),
-                        command.getDate(),
-                        command.getStartTime(),
-                        command.getEndingTime(),
-                        command.getStock(),
-                        command.getService(),
-                        command.getUser(),
-                        command.getIpAddress(),
-                        command.getUserAgent(),
-                        mediator
-                ));
-            }
-        }
-
+        ScheduleDto scheduleDto = scheduleService.findById(command.getId());
+        scheduleDto.setStartTime(command.getStartTime());
+        scheduleDto.setEndingTime(command.getEndingTime());
+        scheduleDto.setDate(command.getDate());
+        scheduleService.update(scheduleDto);
     }
 }
