ac6385f9197a4a53bead0b6a9f477b556f164d2f
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2024-06-23 14:02:45 -0500

    Fix error

--- identity/src/main/java/com/kynsof/identity/infrastructure/services/UserMeServiceImpl.java
+++ identity/src/main/java/com/kynsof/identity/infrastructure/services/UserMeServiceImpl.java
@@ -11,8 +11,6 @@ import com.kynsof.share.core.domain.exception.BusinessNotFoundException;
 import com.kynsof.share.core.domain.exception.DomainErrorMessage;
 import com.kynsof.share.core.domain.exception.GlobalBusinessException;
 import com.kynsof.share.core.domain.response.ErrorField;
-import com.kynsof.share.core.infrastructure.redis.CacheConfig;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -34,7 +32,7 @@ public class UserMeServiceImpl implements IUserMeService {
     }
 
     @Override
-    @Cacheable(cacheNames = CacheConfig.USER_CACHE, unless = "#result == null", key = "#userId")
+   // @Cacheable(cacheNames = CacheConfig.USER_CACHE, unless = "#result == null", key = "#userId")
     public UserMeResponse getUserInfo(UUID userId) {
         var userSystem = repositoryQuery.findById(userId)
                 .orElseThrow(() -> new BusinessNotFoundException(new GlobalBusinessException(
