8b2d066c98650b6fb5c94906282360b349356de8
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2024-07-02 17:06:46 -0500

    Fix error

--- cloudBridges/src/main/java/com/kynsoft/notification/infrastructure/entity/MailjetConfiguration.java
+++ cloudBridges/src/main/java/com/kynsoft/notification/infrastructure/entity/MailjetConfiguration.java
@@ -8,6 +8,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
@@ -38,10 +39,10 @@ public class MailjetConfiguration implements Serializable {
     private String fromName;
 
     @CreationTimestamp
-    @Column(nullable = true, updatable = true)
+    @Column(updatable = false)
     private LocalDateTime createdAt;
 
-    @Column(nullable = true, updatable = true)
+    @UpdateTimestamp
     private LocalDateTime updatedAt;
 
     @OneToMany(mappedBy = "mailjetConfig", cascade = CascadeType.ALL)
