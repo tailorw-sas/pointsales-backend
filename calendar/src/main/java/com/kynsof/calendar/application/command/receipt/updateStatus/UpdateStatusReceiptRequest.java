9ce7ec614952fb0f8083cf0e95f2551974be10f7
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2024-07-04 07:42:21 -0500

    Fix error

--- calendar/src/main/java/com/kynsof/calendar/application/command/receipt/updateStatus/UpdateStatusReceiptRequest.java
+++ calendar/src/main/java/com/kynsof/calendar/application/command/receipt/updateStatus/UpdateStatusReceiptRequest.java
@@ -1,13 +1,11 @@
 package com.kynsof.calendar.application.command.receipt.updateStatus;
 
 import com.kynsof.calendar.domain.dto.enumType.EStatusReceipt;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@AllArgsConstructor
 public class UpdateStatusReceiptRequest {
     private EStatusReceipt statusReceipt;
 
