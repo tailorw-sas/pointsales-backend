d649fd174be460672a72a78ef929800b502b5ca5
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2024-07-09 09:20:54 -0500

    Fix error

--- identity/src/main/java/com/kynsof/identity/application/command/user/changePassword/ChangePasswordCommandHandler.java
+++ identity/src/main/java/com/kynsof/identity/application/command/user/changePassword/ChangePasswordCommandHandler.java
@@ -1,22 +1,27 @@
 package com.kynsof.identity.application.command.user.changePassword;
 
 
+import com.kynsof.identity.domain.dto.UserSystemDto;
 import com.kynsof.identity.domain.interfaces.service.IAuthService;
+import com.kynsof.identity.domain.interfaces.service.IUserSystemService;
 import com.kynsof.share.core.domain.bus.command.ICommandHandler;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ChangePasswordCommandHandler implements ICommandHandler<ChangePasswordCommand> {
     private final IAuthService authService;
+    private final IUserSystemService userSystemService;
 
-    public ChangePasswordCommandHandler(IAuthService authService) {
+    public ChangePasswordCommandHandler(IAuthService authService, IUserSystemService userSystemService) {
 
         this.authService = authService;
+        this.userSystemService = userSystemService;
     }
 
     @Override
     public void handle(ChangePasswordCommand command) {
-        Boolean result = authService.changePassword(command.getId().toString(), command.getNewPassword());
+        UserSystemDto userSystemDto = userSystemService.findById(command.getId());
+        Boolean result = authService.changePassword(userSystemDto.getKeyCloakId().toString(), command.getNewPassword());
         command.setResul(result);
     }
 }
