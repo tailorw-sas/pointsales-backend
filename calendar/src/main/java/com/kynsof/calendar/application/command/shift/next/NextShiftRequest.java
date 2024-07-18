d26f69036425ab51a3abbcf2af22bb07e891ff05
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2024-07-18 15:59:44 -0500

    Fix error

--- calendar/src/main/java/com/kynsof/calendar/application/command/shift/next/NextShiftRequest.java
+++ calendar/src/main/java/com/kynsof/calendar/application/command/shift/next/NextShiftRequest.java
@@ -4,12 +4,10 @@ package com.kynsof.calendar.application.command.shift.next;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.UUID;
-
 @Getter
 @Setter
 public class NextShiftRequest {
-    private UUID local;
-    private UUID service;
-    private UUID doctor;
+    private String local;
+    private String service;
+    private String doctor;
 }
