360f41c5c2f53494a70845a5f38476fe01019c61
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2024-07-19 12:48:18 -0500

    Fix error

--- identity/src/main/java/com/kynsof/identity/application/command/auth/registry/RegistryCommandHandler.java
+++ identity/src/main/java/com/kynsof/identity/application/command/auth/registry/RegistryCommandHandler.java
@@ -3,8 +3,10 @@ package com.kynsof.identity.application.command.auth.registry;
 
 import com.kynsof.identity.domain.interfaces.service.IAuthService;
 import com.kynsof.identity.infrastructure.services.kafka.producer.user.ProducerRegisterUserEventService;
+import com.kynsof.identity.infrastructure.services.kafka.producer.user.welcom.ProducerUserWelcomEventService;
 import com.kynsof.share.core.domain.bus.command.ICommandHandler;
 import com.kynsof.share.core.domain.kafka.entity.UserKafka;
+import com.kynsof.share.core.domain.kafka.entity.UserWelcomKafka;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
@@ -13,10 +15,11 @@ import java.util.UUID;
 public class RegistryCommandHandler implements ICommandHandler<RegistryCommand> {
     private final IAuthService authService;
     private final ProducerRegisterUserEventService producerRegisterUserEventService;
-
-    public RegistryCommandHandler(IAuthService authService, ProducerRegisterUserEventService producerRegisterUserEventService) {
+    private final ProducerUserWelcomEventService producerUserWelcomEventService;
+    public RegistryCommandHandler(IAuthService authService, ProducerRegisterUserEventService producerRegisterUserEventService, ProducerUserWelcomEventService producerUserWelcomEventService) {
         this.authService = authService;
         this.producerRegisterUserEventService = producerRegisterUserEventService;
+        this.producerUserWelcomEventService = producerUserWelcomEventService;
     }
 
     @Override
@@ -34,5 +37,11 @@ public class RegistryCommandHandler implements ICommandHandler<RegistryCommand> 
         userKafka.setFirstname(command.getFirstname());
         userKafka.setLastname(command.getLastname());
         producerRegisterUserEventService.create(userKafka);
+
+        this.producerUserWelcomEventService.create(new UserWelcomKafka(command.getEmail(),
+                command.getPassword(),
+                command.getEmail(),
+                command.getFirstname() + " " + command.getLastname()
+        ));
     }
 }
