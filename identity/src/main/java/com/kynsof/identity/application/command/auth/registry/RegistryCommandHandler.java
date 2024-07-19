caa5763c61b5d72acde7578bac0355a6d42b42d6
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2024-07-19 13:06:00 -0500

    Fix error

--- identity/src/main/java/com/kynsof/identity/application/command/auth/registry/RegistryCommandHandler.java
+++ identity/src/main/java/com/kynsof/identity/application/command/auth/registry/RegistryCommandHandler.java
@@ -1,9 +1,13 @@
 package com.kynsof.identity.application.command.auth.registry;
 
 
+import com.kynsof.identity.domain.dto.UserStatus;
+import com.kynsof.identity.domain.dto.UserSystemDto;
 import com.kynsof.identity.domain.interfaces.service.IAuthService;
+import com.kynsof.identity.domain.interfaces.service.IUserSystemService;
 import com.kynsof.identity.infrastructure.services.kafka.producer.user.ProducerRegisterUserEventService;
 import com.kynsof.identity.infrastructure.services.kafka.producer.user.welcom.ProducerUserWelcomEventService;
+import com.kynsof.share.core.domain.EUserType;
 import com.kynsof.share.core.domain.bus.command.ICommandHandler;
 import com.kynsof.share.core.domain.kafka.entity.UserKafka;
 import com.kynsof.share.core.domain.kafka.entity.UserWelcomKafka;
@@ -16,10 +20,12 @@ public class RegistryCommandHandler implements ICommandHandler<RegistryCommand> 
     private final IAuthService authService;
     private final ProducerRegisterUserEventService producerRegisterUserEventService;
     private final ProducerUserWelcomEventService producerUserWelcomEventService;
-    public RegistryCommandHandler(IAuthService authService, ProducerRegisterUserEventService producerRegisterUserEventService, ProducerUserWelcomEventService producerUserWelcomEventService) {
+    private final IUserSystemService userSystemService;
+    public RegistryCommandHandler(IAuthService authService, ProducerRegisterUserEventService producerRegisterUserEventService, ProducerUserWelcomEventService producerUserWelcomEventService, IUserSystemService userSystemService) {
         this.authService = authService;
         this.producerRegisterUserEventService = producerRegisterUserEventService;
         this.producerUserWelcomEventService = producerUserWelcomEventService;
+        this.userSystemService = userSystemService;
     }
 
     @Override
@@ -38,6 +44,19 @@ public class RegistryCommandHandler implements ICommandHandler<RegistryCommand> 
         userKafka.setLastname(command.getLastname());
         producerRegisterUserEventService.create(userKafka);
 
+        UserSystemDto userDto = new UserSystemDto(
+                UUID.fromString(registerUser),
+                command.getUsername(),
+                command.getEmail(),
+                command.getFirstname(),
+                command.getLastname(),
+                UserStatus.ACTIVE,
+                ""
+        );
+        userDto.setKeyCloakId(UUID.fromString(registerUser));
+        userDto.setUserType(EUserType.PATIENTS);
+
+        UUID id = userSystemService.create(userDto);
         this.producerUserWelcomEventService.create(new UserWelcomKafka(command.getEmail(),
                 command.getPassword(),
                 command.getEmail(),
