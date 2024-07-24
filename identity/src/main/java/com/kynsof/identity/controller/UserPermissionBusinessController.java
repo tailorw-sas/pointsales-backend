fd7065b023b769d525cd7972cf6952f223eb883c
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2024-07-24 06:20:42 -0500

    Fix error

--- identity/src/main/java/com/kynsof/identity/controller/UserPermissionBusinessController.java
+++ identity/src/main/java/com/kynsof/identity/controller/UserPermissionBusinessController.java
@@ -3,14 +3,6 @@ package com.kynsof.identity.controller;
 import com.kynsof.identity.application.command.userPermisionBusiness.create.CreateUserPermissionBusinessCommand;
 import com.kynsof.identity.application.command.userPermisionBusiness.create.CreateUserPermissionBusinessMessage;
 import com.kynsof.identity.application.command.userPermisionBusiness.create.UserPermissionBusinessRequest;
-import com.kynsof.identity.application.command.userPermisionBusiness.delete.DeleteUserRolBusinessCommand;
-import com.kynsof.identity.application.command.userPermisionBusiness.delete.DeleteUserRolBusinessMessage;
-import com.kynsof.identity.application.command.userPermisionBusiness.deleteRelation.DeleteUserBusinessRelationCommand;
-import com.kynsof.identity.application.command.userPermisionBusiness.deleteRelation.DeleteUserBusinessRelationMessage;
-import com.kynsof.identity.application.command.userPermisionBusiness.deleteRelation.DeleteUserBusinessRelationRequest;
-import com.kynsof.identity.application.command.userPermisionBusiness.update.UpdateUserPermissionBusinessCommand;
-import com.kynsof.identity.application.command.userPermisionBusiness.update.UpdateUserPermissionBusinessMessage;
-import com.kynsof.identity.application.command.userPermisionBusiness.update.UserRoleBusinessUpdateRequest;
 import com.kynsof.identity.application.query.userPermissionBusiness.getPermissionsForUserAndBusiness.GetPermissionsForUserAndBusinessQuery;
 import com.kynsof.identity.application.query.userPermissionBusiness.getPermissionsForUserAndBusiness.GetPermissionsForUserAndBusinessResponse;
 import com.kynsof.identity.application.query.userPermissionBusiness.getbyid.FindByIdUserRoleBusinessQuery;
@@ -43,12 +35,6 @@ public class UserPermissionBusinessController {
         return ResponseEntity.ok(response);
     }
 
-    @PatchMapping()
-    public ResponseEntity<?> update(@RequestBody UserRoleBusinessUpdateRequest request) {
-        UpdateUserPermissionBusinessCommand command = UpdateUserPermissionBusinessCommand.fromRequest(request);
-        UpdateUserPermissionBusinessMessage response = mediator.send(command);
-        return ResponseEntity.ok(response);
-    }
 
     @GetMapping(path = "/{id}")
     public ResponseEntity<?> getById(@PathVariable UUID id) {
@@ -59,23 +45,6 @@ public class UserPermissionBusinessController {
         return ResponseEntity.ok(response);
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<DeleteUserRolBusinessMessage> delete(@PathVariable("id") UUID id) {
-
-        DeleteUserRolBusinessCommand command = new DeleteUserRolBusinessCommand(id);
-        DeleteUserRolBusinessMessage response = mediator.send(command);
-
-        return ResponseEntity.ok(response);
-    }
-
-    @DeleteMapping("/delete-relation")
-    public ResponseEntity<?> deleteRelation(@RequestBody DeleteUserBusinessRelationRequest request) {
-
-        DeleteUserBusinessRelationCommand command = new DeleteUserBusinessRelationCommand(request.getUserId(), request.getBusinessId());
-        DeleteUserBusinessRelationMessage response = mediator.send(command);
-
-        return ResponseEntity.ok(response);
-    }
 
     @PostMapping("/search")
     public ResponseEntity<?> search(@RequestBody SearchRequest request) {
