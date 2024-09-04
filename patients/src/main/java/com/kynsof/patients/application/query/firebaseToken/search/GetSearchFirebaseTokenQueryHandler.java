f4f8767d086e95ed2f1f3155b6dfd01b3811e57b
Yannier <70853534+penaescalonayannier@users.noreply.github.com>
2024-09-04 11:54:47 -0400

    Adicionando evento de integracion

---
+++ patients/src/main/java/com/kynsof/patients/application/query/firebaseToken/search/GetSearchFirebaseTokenQueryHandler.java
@@ -0,0 +1,22 @@
+package com.kynsof.patients.application.query.firebaseToken.search;
+
+import com.kynsof.patients.domain.service.IFirebaseTokenService;
+import com.kynsof.share.core.domain.bus.query.IQueryHandler;
+import com.kynsof.share.core.domain.response.PaginatedResponse;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GetSearchFirebaseTokenQueryHandler implements IQueryHandler<GetSearchFirebaseTokenQuery, PaginatedResponse> {
+
+    private final IFirebaseTokenService service;
+
+    public GetSearchFirebaseTokenQueryHandler(IFirebaseTokenService service) {
+        this.service = service;
+    }
+
+    @Override
+    public PaginatedResponse handle(GetSearchFirebaseTokenQuery query) {
+
+        return this.service.search(query.getPageable(), query.getFilter());
+    }
+}
