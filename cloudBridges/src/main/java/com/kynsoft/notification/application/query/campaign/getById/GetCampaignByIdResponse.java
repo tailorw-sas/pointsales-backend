f17df26bd33eea2bc5d20e0c6f2af9f241b37f34
Enrique <enrique.muguercia@gmail.com>
2024-09-07 17:03:18 -0400

    Agregando soport en BD para las campañas

--- cloudBridges/src/main/java/com/kynsoft/notification/application/query/campaign/getById/GetCampaignByIdResponse.java
+++ cloudBridges/src/main/java/com/kynsoft/notification/application/query/campaign/getById/GetCampaignByIdResponse.java
@@ -1,6 +1,16 @@
 package com.kynsoft.notification.application.query.campaign.getById;
 
 import com.kynsof.share.core.domain.bus.query.IResponse;
+import com.kynsoft.notification.domain.dto.CampaignDto;
+import lombok.Getter;
+import org.checkerframework.checker.index.qual.GTENegativeOne;
 
+@Getter
 public class GetCampaignByIdResponse implements IResponse {
+
+    private final CampaignDto campaignDto;
+
+    public GetCampaignByIdResponse(CampaignDto campaignDto) {
+        this.campaignDto = campaignDto;
+    }
 }
