52d75f0783ed0bc8aadb833590dbd594ecc6760c
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2024-09-12 15:34:14 -0500

    Fix error

--- rrhh/src/main/java/com/kynsoft/rrhh/application/command/assistant/create/CreateAssistantCommandHandler.java
+++ rrhh/src/main/java/com/kynsoft/rrhh/application/command/assistant/create/CreateAssistantCommandHandler.java
@@ -1,22 +1,29 @@
 package com.kynsoft.rrhh.application.command.assistant.create;
 
+import com.kynsof.share.core.domain.EUserType;
 import com.kynsof.share.core.domain.RulesChecker;
 import com.kynsof.share.core.domain.bus.command.ICommandHandler;
 import com.kynsof.share.core.domain.kafka.entity.DoctorKafka;
 import com.kynsof.share.core.domain.rules.ValidateObjectNotNullRule;
 import com.kynsoft.rrhh.domain.dto.AssistantDto;
 import com.kynsoft.rrhh.domain.dto.BusinessDto;
+import com.kynsoft.rrhh.domain.dto.CreateUserSystemRequest;
 import com.kynsoft.rrhh.domain.dto.UserBusinessRelationDto;
 import com.kynsoft.rrhh.domain.interfaces.services.IAssistantService;
 import com.kynsoft.rrhh.domain.interfaces.services.IBusinessService;
 import com.kynsoft.rrhh.domain.interfaces.services.IUserBusinessRelationService;
 import com.kynsoft.rrhh.domain.rules.assistant.AssistantEmailMustBeUniqueRule;
 import com.kynsoft.rrhh.domain.rules.assistant.AssistantIdentificationMustBeUniqueRule;
+import com.kynsoft.rrhh.infrastructure.services.UserSystemService;
 import com.kynsoft.rrhh.infrastructure.services.kafka.producer.assistant.ProducerReplicateAssistantService;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.time.LocalDateTime;
 import java.util.UUID;
+
+// Otros imports
 
 @Component
 public class CreateAssistantCommandHandler implements ICommandHandler<CreateAssistantCommand> {
@@ -25,13 +32,20 @@ public class CreateAssistantCommandHandler implements ICommandHandler<CreateAssi
     private final IBusinessService businessService;
     private final IUserBusinessRelationService userBusinessRelationService;
     private final ProducerReplicateAssistantService producerReplicateAssistantService;
+    private final UserSystemService userSystemService;
 
+
+
+// Inyección de RestTemplate
     public CreateAssistantCommandHandler(IAssistantService service, IBusinessService businessService,
-                                         IUserBusinessRelationService userBusinessRelationService, ProducerReplicateAssistantService producerReplicateAssistantService) {
+                                         IUserBusinessRelationService userBusinessRelationService,
+                                         ProducerReplicateAssistantService producerReplicateAssistantService, UserSystemService userSystemService) {
         this.service = service;
         this.businessService = businessService;
         this.userBusinessRelationService = userBusinessRelationService;
         this.producerReplicateAssistantService = producerReplicateAssistantService;
+
+        this.userSystemService = userSystemService;
     }
 
     @Override
@@ -41,25 +55,28 @@ public class CreateAssistantCommandHandler implements ICommandHandler<CreateAssi
         RulesChecker.checkRule(new AssistantEmailMustBeUniqueRule(this.service, command.getEmail()));
         RulesChecker.checkRule(new AssistantIdentificationMustBeUniqueRule(this.service, command.getIdentification()));
         BusinessDto businessDto = this.businessService.findById(command.getBusiness());
-        AssistantDto assistantSave = new AssistantDto(
-                command.getId(),
-                command.getIdentification(),
-                command.getCode(),
-                command.getEmail(),
-                command.getName(),
-                command.getLastName(),
-                command.getStatus(),
-                command.getPhoneNumber(),
-                command.getImage(),
-                command.getDepartment()
-        );
 
-        service.create(assistantSave);
+        try {
+           var id = consumeCreateUserSystemService(command);
+            AssistantDto assistantSave = new AssistantDto(
+                    UUID.fromString(id),
+                    command.getIdentification(),
+                    command.getCode(),
+                    command.getEmail(),
+                    command.getName(),
+                    command.getLastName(),
+                    command.getStatus(),
+                    command.getPhoneNumber(),
+                    command.getImage(),
+                    command.getDepartment()
+            );
 
-        this.userBusinessRelationService.create(new UserBusinessRelationDto(UUID.randomUUID(),
-                assistantSave,businessDto, "ACTIVE", LocalDateTime.now()));
+            service.create(assistantSave);
 
-        producerReplicateAssistantService.create(new DoctorKafka(
+            this.userBusinessRelationService.create(new UserBusinessRelationDto(UUID.randomUUID(),
+                    assistantSave, businessDto, "ACTIVE", LocalDateTime.now()));
+
+            producerReplicateAssistantService.create(new DoctorKafka(
                 assistantSave.getId(),
                 assistantSave.getIdentification(),
                 assistantSave.getCode(),
@@ -69,5 +86,30 @@ public class CreateAssistantCommandHandler implements ICommandHandler<CreateAssi
                 assistantSave.getImage(),
                 command.getBusiness().toString()
         ));
+        } catch (InterruptedException | URISyntaxException | IOException ex) {
+            throw new RuntimeException(ex);
+        }
+
+
+
+
+        // Consumir el servicio createUserSystem desde otro microservicio
+
     }
+
+    // Método para consumir el servicio createUserSystem
+    private String consumeCreateUserSystemService(CreateAssistantCommand command) throws IOException, URISyntaxException, InterruptedException {
+        CreateUserSystemRequest createUserSystemRequest = new CreateUserSystemRequest();
+        createUserSystemRequest.setUserName(command.getEmail());
+        createUserSystemRequest.setEmail(command.getEmail());
+        createUserSystemRequest.setName(command.getName());
+        createUserSystemRequest.setLastName(command.getLastName());
+        createUserSystemRequest.setPassword("defaultPassword"); // Ajusta según tus necesidades
+        createUserSystemRequest.setUserType(EUserType.ASSISTANTS); // Ajusta si es necesario
+        createUserSystemRequest.setImage(command.getImage());
+
+      return userSystemService.createUserSystem(createUserSystemRequest);
+
+    }
+
 }
---
+++ rrhh/src/main/java/com/kynsoft/rrhh/infrastructure/services/UserSystemService.java
@@ -0,0 +1,53 @@
+package com.kynsoft.rrhh.infrastructure.services;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kynsoft.rrhh.domain.dto.CreateUserResponse;
+import com.kynsoft.rrhh.domain.dto.CreateUserSystemRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+@Service
+public class UserSystemService {
+
+    private final HttpClient httpClient;
+    private final ObjectMapper objectMapper;
+
+    // URL del servicio remoto
+    @Value("${user.system.api.url:http://localhost:9909/api/users}")
+    private String userSystemApiUrl;
+
+    public UserSystemService(HttpClient httpClient, ObjectMapper objectMapper) {
+        this.httpClient = httpClient;
+        this.objectMapper = objectMapper;
+    }
+
+    public String createUserSystem(CreateUserSystemRequest value) throws IOException, URISyntaxException, InterruptedException {
+        String requestBody = objectMapper.writeValueAsString(value);
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI(userSystemApiUrl))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(requestBody))
+                .build();
+
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+        if (response.statusCode() != 200) {
+            throw new RuntimeException("Error al crear usuario en el sistema. Código de estado: " + response.statusCode() + ", Respuesta: " + response.body());
+        }
+        if (response.statusCode() != 200) {
+            throw new RuntimeException("Error al crear usuario en el sistema. Código de estado: " + response.statusCode() + ", Respuesta: " + response.body());
+        }
+
+        CreateUserResponse createUserResponse = objectMapper.readValue(response.body(), CreateUserResponse.class);
+
+        return createUserResponse.getId();
+    }
+}
