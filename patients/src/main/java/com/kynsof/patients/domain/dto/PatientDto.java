4e5cd04517f6363ff44ff955f81c2bc5be682daa
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2024-10-10 12:23:32 -0500

    Fix error

--- patients/src/main/java/com/kynsof/patients/domain/dto/PatientDto.java
+++ patients/src/main/java/com/kynsof/patients/domain/dto/PatientDto.java
@@ -14,19 +14,19 @@ import java.util.UUID;
 @Setter
 public class PatientDto implements Serializable {
 
-    private final UUID id;
-    private final String identification;
-    private final String name;
-    private final String lastName;
-    private final GenderType gender;
-    private final Status status;
-    private final Double weight;
-    private final Double height;
-    private final Boolean hasDisability;
-    private final Boolean isPregnant;
-    private final String photo;
-    private final DisabilityType disabilityType;
-    private final int gestationTime;
+    private  UUID id;
+    private  String identification;
+    private  String name;
+    private  String lastName;
+    private  GenderType gender;
+    private  Status status;
+    private  Double weight;
+    private  Double height;
+    private  Boolean hasDisability;
+    private  Boolean isPregnant;
+    private  String photo;
+    private  DisabilityType disabilityType;
+    private  int gestationTime;
 
     private ContactInfoDto contactInfo;
 
