cc0272f6a491621489abe04d51a4ec75013412e4
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2024-11-01 14:12:26 -0500

    Fix error

--- cloudBridges/src/main/java/com/kynsoft/notification/infrastructure/entity/AFile.java
+++ cloudBridges/src/main/java/com/kynsoft/notification/infrastructure/entity/AFile.java
@@ -2,11 +2,16 @@ package com.kynsoft.notification.infrastructure.entity;
 
 import com.kynsof.share.core.domain.BaseEntity;
 import com.kynsoft.notification.domain.dto.AFileDto;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -18,6 +23,13 @@ public class AFile extends BaseEntity {
     private String name;
     private String url;
     private String objetId;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
 
     public AFile(AFileDto file) {
         this.id = file.getId();
