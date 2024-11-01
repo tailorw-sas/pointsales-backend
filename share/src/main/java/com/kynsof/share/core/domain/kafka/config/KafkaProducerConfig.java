759bcbd99963df105cfb53bcf532075c8ad21259
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2024-11-01 12:06:53 -0500

    Fix error

--- share/src/main/java/com/kynsof/share/core/domain/kafka/config/KafkaProducerConfig.java
+++ share/src/main/java/com/kynsof/share/core/domain/kafka/config/KafkaProducerConfig.java
@@ -7,7 +7,6 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
@@ -21,19 +20,19 @@ public class KafkaProducerConfig {
     @Value("${KAFKA_BOOTSTRAP_ADDRESS:localhost:29092}")
     private String bootstrapAddress;
 
-    @Bean
-    @Profile("dev")
-    public ProducerFactory<String, String> devProducerFactory() {
-        Map<String, Object> configProps = createBaseProps();
-        configProps.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SASL_PLAINTEXT");
-        configProps.put(SaslConfigs.SASL_MECHANISM, "PLAIN");
-        configProps.put(SaslConfigs.SASL_JAAS_CONFIG,
-                "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"user1\" password=\"AkC7B1ooWO\";");
-        return new DefaultKafkaProducerFactory<>(configProps);
-    }
+//    @Bean
+//    @Profile("dev")
+//    public ProducerFactory<String, String> devProducerFactory() {
+//        Map<String, Object> configProps = createBaseProps();
+//        configProps.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SASL_PLAINTEXT");
+//        configProps.put(SaslConfigs.SASL_MECHANISM, "PLAIN");
+//        configProps.put(SaslConfigs.SASL_JAAS_CONFIG,
+//                "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"user1\" password=\"AkC7B1ooWO\";");
+//        return new DefaultKafkaProducerFactory<>(configProps);
+//    }
 
     @Bean
-    @Profile("turnero")
+  //  @Profile("turnero")
     public ProducerFactory<String, String> tuneroProducerFactory() {
         Map<String, Object> configProps = createBaseProps();
         configProps.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SASL_PLAINTEXT");
@@ -43,12 +42,12 @@ public class KafkaProducerConfig {
         return new DefaultKafkaProducerFactory<>(configProps);
     }
 
-    @Bean
-    @Profile("!dev")
-    public ProducerFactory<String, String> defaultProducerFactory() {
-        Map<String, Object> configProps = createBaseProps();
-        return new DefaultKafkaProducerFactory<>(configProps);
-    }
+//    @Bean
+//    @Profile("!dev")
+//    public ProducerFactory<String, String> defaultProducerFactory() {
+//        Map<String, Object> configProps = createBaseProps();
+//        return new DefaultKafkaProducerFactory<>(configProps);
+//    }
 
     private Map<String, Object> createBaseProps() {
         Map<String, Object> configProps = new HashMap<>();
