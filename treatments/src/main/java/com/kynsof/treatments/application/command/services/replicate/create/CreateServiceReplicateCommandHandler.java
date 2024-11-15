baec102489bfb65028364d8b1a499ead10c1e960
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2024-11-15 10:44:31 -0500

    Fix error

--- treatments/src/main/java/com/kynsof/treatments/application/command/services/replicate/create/CreateServiceReplicateCommandHandler.java
+++ treatments/src/main/java/com/kynsof/treatments/application/command/services/replicate/create/CreateServiceReplicateCommandHandler.java
@@ -2,25 +2,22 @@ package com.kynsof.treatments.application.command.services.replicate.create;
 
 import com.kynsof.share.core.domain.bus.command.ICommandHandler;
 import com.kynsof.treatments.domain.dto.ServiceDto;
-import com.kynsof.treatments.domain.dto.ServiceTypeDto;
 import com.kynsof.treatments.domain.service.IServiceService;
-import com.kynsof.treatments.domain.service.IServiceTypeService;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CreateServiceReplicateCommandHandler implements ICommandHandler<CreateServiceReplicateCommand> {
 
     private final IServiceService service;
-    private final IServiceTypeService serviceTypeService;
 
-    public CreateServiceReplicateCommandHandler(IServiceService service, IServiceTypeService serviceTypeService) {
+
+    public CreateServiceReplicateCommandHandler(IServiceService service) {
         this.service = service;
-        this.serviceTypeService = serviceTypeService;
+
     }
 
     @Override
     public void handle(CreateServiceReplicateCommand command) {
-        ServiceTypeDto serviceTypeDto = serviceTypeService.findById(command.getType());
 
         ServiceDto serviceDto = service.create(new ServiceDto(
                 command.getId(),
