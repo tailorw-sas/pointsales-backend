bb3d1f7da7b8068635cd965180f9ca4a1605a0b2
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2024-12-06 15:00:26 -0500

    Fix error

--- report/src/main/java/com/kynsoft/report/controller/ReportController.java
+++ report/src/main/java/com/kynsoft/report/controller/ReportController.java
@@ -9,11 +9,7 @@ import com.kynsoft.report.applications.command.generateTemplate1.GenerateTemplat
 import com.kynsoft.report.applications.command.generateTemplate1.GenerateTemplateRequest;
 import com.kynsoft.report.applications.query.reportTemplate.GetReportParameterByCodeQuery;
 import com.kynsoft.report.applications.query.reportTemplate.GetReportParameterByCodeResponse;
-import jakarta.validation.Valid;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,7 +19,7 @@ import org.springframework.web.bind.annotation.*;
 public class ReportController {
 
     private final IMediator mediator;
-    private static final Logger logger = LoggerFactory.getLogger(ReportController.class);
+
     public ReportController(IMediator mediator) {
         this.mediator = mediator;
     }
@@ -40,30 +36,17 @@ public class ReportController {
                 .body(response.getResult());
     }
 
-    @PostMapping(value = "/generate-report", produces = {MediaType.APPLICATION_PDF_VALUE, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"})
-    public ResponseEntity<?> generatedReport(@Valid @RequestBody GenerateReportRequest request) {
-
-        logger.info("Request received to generate report: {}", request);
+    @PostMapping(value = "/generate", produces = MediaType.APPLICATION_PDF_VALUE)
+    public ResponseEntity<?> generatedReport(@RequestBody GenerateReportRequest request) {
 
         GenerateReportCommand command = new GenerateReportCommand(request.getParameters(),
                 request.getJasperReportCode(), request.getReportFormatType());
+        GenerateReportMessage response = mediator.send(command);
 
-        try {
-            GenerateReportMessage response = mediator.send(command);
-
-            MediaType contentType = "XLS".equalsIgnoreCase(request.getReportFormatType())
-                    ? MediaType.parseMediaType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
-                    : MediaType.APPLICATION_PDF;
-
-            return ResponseEntity.ok()
-                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=report." + request.getReportFormatType().toLowerCase())
-                    .header(HttpHeaders.CACHE_CONTROL, "no-cache, no-store, must-revalidate")
-                    .contentType(contentType)
-                    .body(response.getResult());
-        } catch (Exception e) {
-            logger.error("Error generating report: {}", e.getMessage(), e);
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error generating report: " + e.getMessage());
-        }
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=report.pdf")
+                .contentType(MediaType.APPLICATION_PDF)
+                .body(response.getResult());
     }
 
 
