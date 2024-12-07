fd95c39d591ccc464dfa68bdff2e004e46ed4b86
kmontes198910 <67848770+kmontes198910@users.noreply.github.com>
2024-12-07 13:14:56 -0500

    Fix error

--- report/src/main/java/com/kynsoft/report/controller/ReportController.java
+++ report/src/main/java/com/kynsoft/report/controller/ReportController.java
@@ -9,11 +9,14 @@ import com.kynsoft.report.applications.command.generateTemplate1.GenerateTemplat
 import com.kynsoft.report.applications.command.generateTemplate1.GenerateTemplateRequest;
 import com.kynsoft.report.applications.query.reportTemplate.GetReportParameterByCodeQuery;
 import com.kynsoft.report.applications.query.reportTemplate.GetReportParameterByCodeResponse;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
+
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/reports")
@@ -37,17 +40,37 @@ public class ReportController {
                 .body(response.getResult());
     }
 
-    @PostMapping(value = "/generate", produces = MediaType.APPLICATION_PDF_VALUE)
-    public Mono<ResponseEntity<byte[]>> generateReport(@RequestBody GenerateReportRequest request) {
+//    @PostMapping(value = "/generate", produces = MediaType.APPLICATION_PDF_VALUE)
+//    public Mono<ResponseEntity<byte[]>> generateReport(@RequestBody GenerateReportRequest request) {
+//        return Mono.fromCallable(() -> {
+//            GenerateReportCommand command = new GenerateReportCommand(request.getParameters(),
+//                    request.getJasperReportCode(), request.getReportFormatType());
+//            GenerateReportMessage response = mediator.send(command);
+//
+//            return ResponseEntity.ok()
+//                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=report.pdf")
+//                    .contentType(MediaType.APPLICATION_PDF)
+//                    .body(response.getResult());
+//        });
+//    }
+
+    @PostMapping(value = "/generate", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<ResponseEntity<Map<String, String>>> generateReport(@RequestBody GenerateReportRequest request) {
         return Mono.fromCallable(() -> {
             GenerateReportCommand command = new GenerateReportCommand(request.getParameters(),
                     request.getJasperReportCode(), request.getReportFormatType());
             GenerateReportMessage response = mediator.send(command);
 
+            // Convertir el resultado (PDF) a Base64
+            String base64Report = Base64.getEncoder().encodeToString(response.getResult());
+
+            // Construir la respuesta como un JSON
+            Map<String, String> jsonResponse = new HashMap<>();
+            jsonResponse.put("base64Report", base64Report);
+
             return ResponseEntity.ok()
-                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=report.pdf")
-                    .contentType(MediaType.APPLICATION_PDF)
-                    .body(response.getResult());
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(jsonResponse);
         });
     }
 
